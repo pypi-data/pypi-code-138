72930a279f03533670e9fdd20b5fbeefee6cf094
Tom Forbes <tom@tomforb.es>
2022-10-28 16:49:53 +0000

    {"name":"hefan-baogao-2018-2019","version":"2022.10.29.0","file":"hefan_baogao_2018_2019-2022.10.29.0-py3-none-any.whl","path":"hefan-baogao-2018-2019/hefan_baogao_2018_2019-2022.10.29.0-py3-none-any.whl"}

---
+++ hefan-baogao-2018-2019/hefan_baogao_2018_2019-2022.10.29.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""hefan-baogao-2018-2019
+https://github.com/apachecn/hefan-baogao-2018-2019"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.29.0"
