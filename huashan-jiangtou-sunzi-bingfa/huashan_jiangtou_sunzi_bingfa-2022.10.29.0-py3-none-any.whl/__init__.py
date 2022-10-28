74c18a33006e3dce7df2dd296481d25d0334dbae
Tom Forbes <tom@tomforb.es>
2022-10-28 16:50:06 +0000

    {"name":"huashan-jiangtou-sunzi-bingfa","version":"2022.10.29.0","file":"huashan_jiangtou_sunzi_bingfa-2022.10.29.0-py3-none-any.whl","path":"huashan-jiangtou-sunzi-bingfa/huashan_jiangtou_sunzi_bingfa-2022.10.29.0-py3-none-any.whl"}

---
+++ huashan-jiangtou-sunzi-bingfa/huashan_jiangtou_sunzi_bingfa-2022.10.29.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""huashan-jiangtou-sunzi-bingfa
+https://github.com/apachecn/huashan-jiangtou-sunzi-bingfa"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.29.0"
