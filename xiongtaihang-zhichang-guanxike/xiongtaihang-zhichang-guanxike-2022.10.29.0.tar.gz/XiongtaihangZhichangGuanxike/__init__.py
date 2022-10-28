d99a7dc788e48287818405288d1f695283567729
Tom Forbes <tom@tomforb.es>
2022-10-28 17:32:28 +0000

    {"name":"xiongtaihang-zhichang-guanxike","version":"2022.10.29.0","file":"xiongtaihang-zhichang-guanxike-2022.10.29.0.tar.gz","path":"xiongtaihang-zhichang-guanxike/xiongtaihang-zhichang-guanxike-2022.10.29.0.tar.gz"}

---
+++ xiongtaihang-zhichang-guanxike/xiongtaihang-zhichang-guanxike-2022.10.29.0.tar.gz/XiongtaihangZhichangGuanxike/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""xiongtaihang-zhichang-guanxike
+https://github.com/apachecn/xiongtaihang-zhichang-guanxike"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.29.0"
