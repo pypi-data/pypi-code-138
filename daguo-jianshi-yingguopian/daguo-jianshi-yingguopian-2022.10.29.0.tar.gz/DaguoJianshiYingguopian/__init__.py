18334d1af9c7e20040e71419a8038ad11e240ddd
Tom Forbes <tom@tomforb.es>
2022-10-28 16:47:05 +0000

    {"name":"daguo-jianshi-yingguopian","version":"2022.10.29.0","file":"daguo-jianshi-yingguopian-2022.10.29.0.tar.gz","path":"daguo-jianshi-yingguopian/daguo-jianshi-yingguopian-2022.10.29.0.tar.gz"}

---
+++ daguo-jianshi-yingguopian/daguo-jianshi-yingguopian-2022.10.29.0.tar.gz/DaguoJianshiYingguopian/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""daguo-jianshi-yingguopian
+https://github.com/apachecn/daguo-jianshi-yingguopian"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.29.0"
