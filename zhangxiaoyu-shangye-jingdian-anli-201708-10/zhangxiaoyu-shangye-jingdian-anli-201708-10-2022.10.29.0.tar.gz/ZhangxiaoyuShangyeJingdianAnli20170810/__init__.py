d78aee851c6a8b8df8495567b99412d35816f30c
Tom Forbes <tom@tomforb.es>
2022-10-28 17:37:45 +0000

    {"name":"zhangxiaoyu-shangye-jingdian-anli-201708-10","version":"2022.10.29.0","file":"zhangxiaoyu-shangye-jingdian-anli-201708-10-2022.10.29.0.tar.gz","path":"zhangxiaoyu-shangye-jingdian-anli-201708-10/zhangxiaoyu-shangye-jingdian-anli-201708-10-2022.10.29.0.tar.gz"}

---
+++ zhangxiaoyu-shangye-jingdian-anli-201708-10/zhangxiaoyu-shangye-jingdian-anli-201708-10-2022.10.29.0.tar.gz/ZhangxiaoyuShangyeJingdianAnli20170810/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""zhangxiaoyu-shangye-jingdian-anli-201708-10
+https://github.com/apachecn/zhangxiaoyu-shangye-jingdian-anli-201708-10"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.29.0"
