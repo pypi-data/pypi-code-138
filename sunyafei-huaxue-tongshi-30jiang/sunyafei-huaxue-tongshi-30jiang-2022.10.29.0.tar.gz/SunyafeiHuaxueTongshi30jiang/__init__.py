032b848391ad2614987a6da931251a0e1b92e88a
Tom Forbes <tom@tomforb.es>
2022-10-28 17:16:38 +0000

    {"name":"sunyafei-huaxue-tongshi-30jiang","version":"2022.10.29.0","file":"sunyafei-huaxue-tongshi-30jiang-2022.10.29.0.tar.gz","path":"sunyafei-huaxue-tongshi-30jiang/sunyafei-huaxue-tongshi-30jiang-2022.10.29.0.tar.gz"}

---
+++ sunyafei-huaxue-tongshi-30jiang/sunyafei-huaxue-tongshi-30jiang-2022.10.29.0.tar.gz/SunyafeiHuaxueTongshi30jiang/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""sunyafei-huaxue-tongshi-30jiang
+https://github.com/apachecn/sunyafei-huaxue-tongshi-30jiang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.29.0"
