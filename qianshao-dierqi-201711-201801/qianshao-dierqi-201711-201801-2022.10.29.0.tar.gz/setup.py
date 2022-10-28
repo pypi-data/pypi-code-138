2b948f3cf756a8b12408adba0543d2eedcb802e1
Tom Forbes <tom@tomforb.es>
2022-10-28 17:10:35 +0000

    {"name":"qianshao-dierqi-201711-201801","version":"2022.10.29.0","file":"qianshao-dierqi-201711-201801-2022.10.29.0.tar.gz","path":"qianshao-dierqi-201711-201801/qianshao-dierqi-201711-201801-2022.10.29.0.tar.gz"}

---
+++ qianshao-dierqi-201711-201801/qianshao-dierqi-201711-201801-2022.10.29.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import QianshaoDierqi201711201801
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('QianshaoDierqi201711201801'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="qianshao-dierqi-201711-201801",
+    version=QianshaoDierqi201711201801.__version__,
+    url="https://github.com/apachecn/qianshao-dierqi-201711-201801",
+    author=QianshaoDierqi201711201801.__author__,
+    author_email=QianshaoDierqi201711201801.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="前哨第二期201711-201801",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "qianshao-dierqi-201711-201801=QianshaoDierqi201711201801.__main__:main",
+            "QianshaoDierqi201711201801=QianshaoDierqi201711201801.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
