2c31db9dcc7f4fd9ac9a9b8deee7106962653c48
Tom Forbes <tom@tomforb.es>
2022-10-28 17:14:19 +0000

    {"name":"shehuixue-zhuanti-50jiang","version":"2022.10.29.0","file":"shehuixue_zhuanti_50jiang-2022.10.29.0-py3-none-any.whl","path":"shehuixue-zhuanti-50jiang/shehuixue_zhuanti_50jiang-2022.10.29.0-py3-none-any.whl"}

---
+++ shehuixue-zhuanti-50jiang/shehuixue_zhuanti_50jiang-2022.10.29.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shehuixue-zhuanti-50jiang
+https://github.com/apachecn/shehuixue-zhuanti-50jiang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.29.0"
