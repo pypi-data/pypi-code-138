b16d634debfc842fbb3c9e277df098072f8f09d8
Tom Forbes <tom@tomforb.es>
2022-10-28 17:43:04 +0000

    {"name":"zhuokede-mimaxueke","version":"2022.10.29.0","file":"zhuokede-mimaxueke-2022.10.29.0.tar.gz","path":"zhuokede-mimaxueke/zhuokede-mimaxueke-2022.10.29.0.tar.gz"}

---
+++ zhuokede-mimaxueke/zhuokede-mimaxueke-2022.10.29.0.tar.gz/ZhuokedeMimaxueke/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""zhuokede-mimaxueke
+https://github.com/apachecn/zhuokede-mimaxueke"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.29.0"
