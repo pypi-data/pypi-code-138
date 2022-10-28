e35a72c4c407cf18196b1023b1d95b9f17f88829
Tom Forbes <tom@tomforb.es>
2022-10-28 17:18:36 +0000

    {"name":"wanweigang-jingying-rike-201608-201610","version":"2022.10.29.0","file":"wanweigang-jingying-rike-201608-201610-2022.10.29.0.tar.gz","path":"wanweigang-jingying-rike-201608-201610/wanweigang-jingying-rike-201608-201610-2022.10.29.0.tar.gz"}

---
+++ wanweigang-jingying-rike-201608-201610/wanweigang-jingying-rike-201608-201610-2022.10.29.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import WanweigangJingyingRike201608201610
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('WanweigangJingyingRike201608201610'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="wanweigang-jingying-rike-201608-201610",
+    version=WanweigangJingyingRike201608201610.__version__,
+    url="https://github.com/apachecn/wanweigang-jingying-rike-201608-201610",
+    author=WanweigangJingyingRike201608201610.__author__,
+    author_email=WanweigangJingyingRike201608201610.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="万维钢精英日课201608-201610",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "wanweigang-jingying-rike-201608-201610=WanweigangJingyingRike201608201610.__main__:main",
+            "WanweigangJingyingRike201608201610=WanweigangJingyingRike201608201610.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
