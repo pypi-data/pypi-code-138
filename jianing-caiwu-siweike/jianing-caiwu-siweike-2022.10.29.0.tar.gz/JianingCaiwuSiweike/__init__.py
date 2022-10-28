2c79566affa10282eda1dc653722d0dcf4e30775
Tom Forbes <tom@tomforb.es>
2022-10-28 16:50:29 +0000

    {"name":"jianing-caiwu-siweike","version":"2022.10.29.0","file":"jianing-caiwu-siweike-2022.10.29.0.tar.gz","path":"jianing-caiwu-siweike/jianing-caiwu-siweike-2022.10.29.0.tar.gz"}

---
+++ jianing-caiwu-siweike/jianing-caiwu-siweike-2022.10.29.0.tar.gz/JianingCaiwuSiweike/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jianing-caiwu-siweike
+https://github.com/apachecn/jianing-caiwu-siweike"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.29.0"
