6230c0987a70dbb49a8e3e39fc60ba19ebb54285
Tom Forbes <tom@tomforb.es>
2022-10-28 17:41:20 +0000

    {"name":"zhuoke-kexue-siwei-bixiuke-201712-201802","version":"2022.10.29.0","file":"zhuoke-kexue-siwei-bixiuke-201712-201802-2022.10.29.0.tar.gz","path":"zhuoke-kexue-siwei-bixiuke-201712-201802/zhuoke-kexue-siwei-bixiuke-201712-201802-2022.10.29.0.tar.gz"}

---
+++ zhuoke-kexue-siwei-bixiuke-201712-201802/zhuoke-kexue-siwei-bixiuke-201712-201802-2022.10.29.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import ZhuokeKexueSiweiBixiuke201712201802
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('ZhuokeKexueSiweiBixiuke201712201802'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="zhuoke-kexue-siwei-bixiuke-201712-201802",
+    version=ZhuokeKexueSiweiBixiuke201712201802.__version__,
+    url="https://github.com/apachecn/zhuoke-kexue-siwei-bixiuke-201712-201802",
+    author=ZhuokeKexueSiweiBixiuke201712201802.__author__,
+    author_email=ZhuokeKexueSiweiBixiuke201712201802.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="卓克科学思维必修课201712-201802",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "zhuoke-kexue-siwei-bixiuke-201712-201802=ZhuokeKexueSiweiBixiuke201712201802.__main__:main",
+            "ZhuokeKexueSiweiBixiuke201712201802=ZhuokeKexueSiweiBixiuke201712201802.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
