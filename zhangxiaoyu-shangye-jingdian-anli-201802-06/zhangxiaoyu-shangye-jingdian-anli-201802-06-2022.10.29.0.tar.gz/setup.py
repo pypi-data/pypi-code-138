8522a4012da13183ed27d204aa1505fb3027a065
Tom Forbes <tom@tomforb.es>
2022-10-28 17:38:57 +0000

    {"name":"zhangxiaoyu-shangye-jingdian-anli-201802-06","version":"2022.10.29.0","file":"zhangxiaoyu-shangye-jingdian-anli-201802-06-2022.10.29.0.tar.gz","path":"zhangxiaoyu-shangye-jingdian-anli-201802-06/zhangxiaoyu-shangye-jingdian-anli-201802-06-2022.10.29.0.tar.gz"}

---
+++ zhangxiaoyu-shangye-jingdian-anli-201802-06/zhangxiaoyu-shangye-jingdian-anli-201802-06-2022.10.29.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import ZhangxiaoyuShangyeJingdianAnli20180206
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('ZhangxiaoyuShangyeJingdianAnli20180206'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="zhangxiaoyu-shangye-jingdian-anli-201802-06",
+    version=ZhangxiaoyuShangyeJingdianAnli20180206.__version__,
+    url="https://github.com/apachecn/zhangxiaoyu-shangye-jingdian-anli-201802-06",
+    author=ZhangxiaoyuShangyeJingdianAnli20180206.__author__,
+    author_email=ZhangxiaoyuShangyeJingdianAnli20180206.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="张潇雨商业经典案例201802-06",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "zhangxiaoyu-shangye-jingdian-anli-201802-06=ZhangxiaoyuShangyeJingdianAnli20180206.__main__:main",
+            "ZhangxiaoyuShangyeJingdianAnli20180206=ZhangxiaoyuShangyeJingdianAnli20180206.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
