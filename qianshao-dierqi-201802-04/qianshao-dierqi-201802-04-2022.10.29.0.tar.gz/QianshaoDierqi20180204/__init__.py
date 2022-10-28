8c20d62065c6908b506c16a257a3410dc2b5f8a7
Tom Forbes <tom@tomforb.es>
2022-10-28 17:11:05 +0000

    {"name":"qianshao-dierqi-201802-04","version":"2022.10.29.0","file":"qianshao-dierqi-201802-04-2022.10.29.0.tar.gz","path":"qianshao-dierqi-201802-04/qianshao-dierqi-201802-04-2022.10.29.0.tar.gz"}

---
+++ qianshao-dierqi-201802-04/qianshao-dierqi-201802-04-2022.10.29.0.tar.gz/QianshaoDierqi20180204/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""qianshao-dierqi-201802-04
+https://github.com/apachecn/qianshao-dierqi-201802-04"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.29.0"
