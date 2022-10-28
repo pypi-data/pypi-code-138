ccfe9350c23ff8ff760e53c0e7325cb7468b769c
Tom Forbes <tom@tomforb.es>
2022-10-28 16:47:35 +0000

    {"name":"drweide-jiaoyu-baodian-201709-201803","version":"2022.10.29.0","file":"drweide_jiaoyu_baodian_201709_201803-2022.10.29.0-py3-none-any.whl","path":"drweide-jiaoyu-baodian-201709-201803/drweide_jiaoyu_baodian_201709_201803-2022.10.29.0-py3-none-any.whl"}

---
+++ drweide-jiaoyu-baodian-201709-201803/drweide_jiaoyu_baodian_201709_201803-2022.10.29.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""drweide-jiaoyu-baodian-201709-201803
+https://github.com/apachecn/drweide-jiaoyu-baodian-201709-201803"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.29.0"
