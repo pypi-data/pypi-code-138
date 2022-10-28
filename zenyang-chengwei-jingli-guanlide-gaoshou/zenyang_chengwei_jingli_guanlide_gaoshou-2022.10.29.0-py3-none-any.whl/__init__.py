2822cd067dc7a871fcbf71a9a47f9eae58c423d9
Tom Forbes <tom@tomforb.es>
2022-10-28 17:36:06 +0000

    {"name":"zenyang-chengwei-jingli-guanlide-gaoshou","version":"2022.10.29.0","file":"zenyang_chengwei_jingli_guanlide_gaoshou-2022.10.29.0-py3-none-any.whl","path":"zenyang-chengwei-jingli-guanlide-gaoshou/zenyang_chengwei_jingli_guanlide_gaoshou-2022.10.29.0-py3-none-any.whl"}

---
+++ zenyang-chengwei-jingli-guanlide-gaoshou/zenyang_chengwei_jingli_guanlide_gaoshou-2022.10.29.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""zenyang-chengwei-jingli-guanlide-gaoshou
+https://github.com/apachecn/zenyang-chengwei-jingli-guanlide-gaoshou"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.29.0"
