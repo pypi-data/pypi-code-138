78c883043f1e043c17a955d5c793a5a742b02c34
Tom Forbes <tom@tomforb.es>
2022-10-28 17:26:11 +0000

    {"name":"wuzhihong-xinlixueke-201804-07","version":"2022.10.29.0","file":"wuzhihong-xinlixueke-201804-07-2022.10.29.0.tar.gz","path":"wuzhihong-xinlixueke-201804-07/wuzhihong-xinlixueke-201804-07-2022.10.29.0.tar.gz"}

---
+++ wuzhihong-xinlixueke-201804-07/wuzhihong-xinlixueke-201804-07-2022.10.29.0.tar.gz/WuzhihongXinlixueke20180407/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""wuzhihong-xinlixueke-201804-07
+https://github.com/apachecn/wuzhihong-xinlixueke-201804-07"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.29.0"
