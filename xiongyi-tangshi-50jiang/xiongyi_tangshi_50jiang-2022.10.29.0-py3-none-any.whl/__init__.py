036dd3069a2bb4c3205519275980654a5faa90b4
Tom Forbes <tom@tomforb.es>
2022-10-28 17:33:30 +0000

    {"name":"xiongyi-tangshi-50jiang","version":"2022.10.29.0","file":"xiongyi_tangshi_50jiang-2022.10.29.0-py3-none-any.whl","path":"xiongyi-tangshi-50jiang/xiongyi_tangshi_50jiang-2022.10.29.0-py3-none-any.whl"}

---
+++ xiongyi-tangshi-50jiang/xiongyi_tangshi_50jiang-2022.10.29.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""xiongyi-tangshi-50jiang
+https://github.com/apachecn/xiongyi-tangshi-50jiang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.29.0"
