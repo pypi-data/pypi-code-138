d51ef6893a9f33472b1abdb206c0cb63d43c5f31
Tom Forbes <tom@tomforb.es>
2022-10-28 16:49:17 +0000

    {"name":"gudian-chaoji-geti-201704-06","version":"2022.10.29.0","file":"gudian-chaoji-geti-201704-06-2022.10.29.0.tar.gz","path":"gudian-chaoji-geti-201704-06/gudian-chaoji-geti-201704-06-2022.10.29.0.tar.gz"}

---
+++ gudian-chaoji-geti-201704-06/gudian-chaoji-geti-201704-06-2022.10.29.0.tar.gz/GudianChaojiGeti20170406/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""gudian-chaoji-geti-201704-06
+https://github.com/apachecn/gudian-chaoji-geti-201704-06"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.29.0"
