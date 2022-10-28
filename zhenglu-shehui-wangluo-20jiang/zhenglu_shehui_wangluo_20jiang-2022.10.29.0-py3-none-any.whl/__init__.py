e2f15b4cd31c21453c47edecd40c47d41b0e22fd
Tom Forbes <tom@tomforb.es>
2022-10-28 17:39:13 +0000

    {"name":"zhenglu-shehui-wangluo-20jiang","version":"2022.10.29.0","file":"zhenglu_shehui_wangluo_20jiang-2022.10.29.0-py3-none-any.whl","path":"zhenglu-shehui-wangluo-20jiang/zhenglu_shehui_wangluo_20jiang-2022.10.29.0-py3-none-any.whl"}

---
+++ zhenglu-shehui-wangluo-20jiang/zhenglu_shehui_wangluo_20jiang-2022.10.29.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""zhenglu-shehui-wangluo-20jiang
+https://github.com/apachecn/zhenglu-shehui-wangluo-20jiang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.29.0"
