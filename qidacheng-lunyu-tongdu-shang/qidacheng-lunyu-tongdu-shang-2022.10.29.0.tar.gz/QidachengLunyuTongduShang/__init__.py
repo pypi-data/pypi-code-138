d7eaae167359f9ce9e36cd911bf9e5d3e12f6b30
Tom Forbes <tom@tomforb.es>
2022-10-28 17:12:06 +0000

    {"name":"qidacheng-lunyu-tongdu-shang","version":"2022.10.29.0","file":"qidacheng-lunyu-tongdu-shang-2022.10.29.0.tar.gz","path":"qidacheng-lunyu-tongdu-shang/qidacheng-lunyu-tongdu-shang-2022.10.29.0.tar.gz"}

---
+++ qidacheng-lunyu-tongdu-shang/qidacheng-lunyu-tongdu-shang-2022.10.29.0.tar.gz/QidachengLunyuTongduShang/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""qidacheng-lunyu-tongdu-shang
+https://github.com/apachecn/qidacheng-lunyu-tongdu-shang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.29.0"
