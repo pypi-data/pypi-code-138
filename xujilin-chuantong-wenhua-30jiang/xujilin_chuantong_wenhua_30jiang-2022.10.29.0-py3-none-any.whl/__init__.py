ab74e6ba0407092bbe72ceeb7eb03d6967fe13bc
Tom Forbes <tom@tomforb.es>
2022-10-28 17:34:40 +0000

    {"name":"xujilin-chuantong-wenhua-30jiang","version":"2022.10.29.0","file":"xujilin_chuantong_wenhua_30jiang-2022.10.29.0-py3-none-any.whl","path":"xujilin-chuantong-wenhua-30jiang/xujilin_chuantong_wenhua_30jiang-2022.10.29.0-py3-none-any.whl"}

---
+++ xujilin-chuantong-wenhua-30jiang/xujilin_chuantong_wenhua_30jiang-2022.10.29.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""xujilin-chuantong-wenhua-30jiang
+https://github.com/apachecn/xujilin-chuantong-wenhua-30jiang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.29.0"
