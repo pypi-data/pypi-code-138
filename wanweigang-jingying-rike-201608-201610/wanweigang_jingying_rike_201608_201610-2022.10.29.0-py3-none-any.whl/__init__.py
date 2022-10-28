3cf60ea36236c31e021298bb7ea302eb2b612a83
Tom Forbes <tom@tomforb.es>
2022-10-28 17:18:32 +0000

    {"name":"wanweigang-jingying-rike-201608-201610","version":"2022.10.29.0","file":"wanweigang_jingying_rike_201608_201610-2022.10.29.0-py3-none-any.whl","path":"wanweigang-jingying-rike-201608-201610/wanweigang_jingying_rike_201608_201610-2022.10.29.0-py3-none-any.whl"}

---
+++ wanweigang-jingying-rike-201608-201610/wanweigang_jingying_rike_201608_201610-2022.10.29.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""wanweigang-jingying-rike-201608-201610
+https://github.com/apachecn/wanweigang-jingying-rike-201608-201610"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.29.0"
