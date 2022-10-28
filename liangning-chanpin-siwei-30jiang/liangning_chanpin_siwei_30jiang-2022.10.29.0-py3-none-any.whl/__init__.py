c1c46ee92df29ce55293f033bb2d39b7d08dd3d2
Tom Forbes <tom@tomforb.es>
2022-10-28 16:51:43 +0000

    {"name":"liangning-chanpin-siwei-30jiang","version":"2022.10.29.0","file":"liangning_chanpin_siwei_30jiang-2022.10.29.0-py3-none-any.whl","path":"liangning-chanpin-siwei-30jiang/liangning_chanpin_siwei_30jiang-2022.10.29.0-py3-none-any.whl"}

---
+++ liangning-chanpin-siwei-30jiang/liangning_chanpin_siwei_30jiang-2022.10.29.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""liangning-chanpin-siwei-30jiang
+https://github.com/apachecn/liangning-chanpin-siwei-30jiang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.29.0"
