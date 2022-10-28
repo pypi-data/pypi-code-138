1d07a7d32e6c1efe81bb0227c47e733fd570bdff
Tom Forbes <tom@tomforb.es>
2022-10-28 16:45:56 +0000

    {"name":"bofan-rizhilu-201701-201703","version":"2022.10.29.0","file":"bofan-rizhilu-201701-201703-2022.10.29.0.tar.gz","path":"bofan-rizhilu-201701-201703/bofan-rizhilu-201701-201703-2022.10.29.0.tar.gz"}

---
+++ bofan-rizhilu-201701-201703/bofan-rizhilu-201701-201703-2022.10.29.0.tar.gz/BofanRizhilu201701201703/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""bofan-rizhilu-201701-201703
+https://github.com/apachecn/bofan-rizhilu-201701-201703"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.29.0"
