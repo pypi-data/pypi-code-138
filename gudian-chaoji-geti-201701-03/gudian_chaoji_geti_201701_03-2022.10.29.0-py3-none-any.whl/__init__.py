b8099e5b47aef858fa3e98773f8e9ee2638818f3
Tom Forbes <tom@tomforb.es>
2022-10-28 16:48:58 +0000

    {"name":"gudian-chaoji-geti-201701-03","version":"2022.10.29.0","file":"gudian_chaoji_geti_201701_03-2022.10.29.0-py3-none-any.whl","path":"gudian-chaoji-geti-201701-03/gudian_chaoji_geti_201701_03-2022.10.29.0-py3-none-any.whl"}

---
+++ gudian-chaoji-geti-201701-03/gudian_chaoji_geti_201701_03-2022.10.29.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""gudian-chaoji-geti-201701-03
+https://github.com/apachecn/gudian-chaoji-geti-201701-03"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.29.0"
