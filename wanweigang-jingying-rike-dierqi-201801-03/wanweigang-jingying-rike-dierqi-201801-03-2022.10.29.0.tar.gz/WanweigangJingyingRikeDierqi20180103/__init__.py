f0b8e937407ea758a652e631f724c750d640d1d7
Tom Forbes <tom@tomforb.es>
2022-10-28 17:20:57 +0000

    {"name":"wanweigang-jingying-rike-dierqi-201801-03","version":"2022.10.29.0","file":"wanweigang-jingying-rike-dierqi-201801-03-2022.10.29.0.tar.gz","path":"wanweigang-jingying-rike-dierqi-201801-03/wanweigang-jingying-rike-dierqi-201801-03-2022.10.29.0.tar.gz"}

---
+++ wanweigang-jingying-rike-dierqi-201801-03/wanweigang-jingying-rike-dierqi-201801-03-2022.10.29.0.tar.gz/WanweigangJingyingRikeDierqi20180103/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""wanweigang-jingying-rike-dierqi-201801-03
+https://github.com/apachecn/wanweigang-jingying-rike-dierqi-201801-03"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.29.0"
