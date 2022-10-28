de6fad55b94a9b7081b43d9aa6ae3439f65563d4
Tom Forbes <tom@tomforb.es>
2022-10-28 17:26:36 +0000

    {"name":"xiangshua-beida-jinrongxue-jiacan","version":"2022.10.29.0","file":"xiangshua-beida-jinrongxue-jiacan-2022.10.29.0.tar.gz","path":"xiangshua-beida-jinrongxue-jiacan/xiangshua-beida-jinrongxue-jiacan-2022.10.29.0.tar.gz"}

---
+++ xiangshua-beida-jinrongxue-jiacan/xiangshua-beida-jinrongxue-jiacan-2022.10.29.0.tar.gz/XiangshuaBeidaJinrongxueJiacan/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""xiangshua-beida-jinrongxue-jiacan
+https://github.com/apachecn/xiangshua-beida-jinrongxue-jiacan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.29.0"
