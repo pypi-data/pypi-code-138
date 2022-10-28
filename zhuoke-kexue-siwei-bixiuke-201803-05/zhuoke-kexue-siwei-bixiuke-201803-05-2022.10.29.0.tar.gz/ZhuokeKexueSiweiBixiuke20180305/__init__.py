62497f21dc17927bd2b3cdc999350720554f3b16
Tom Forbes <tom@tomforb.es>
2022-10-28 17:41:54 +0000

    {"name":"zhuoke-kexue-siwei-bixiuke-201803-05","version":"2022.10.29.0","file":"zhuoke-kexue-siwei-bixiuke-201803-05-2022.10.29.0.tar.gz","path":"zhuoke-kexue-siwei-bixiuke-201803-05/zhuoke-kexue-siwei-bixiuke-201803-05-2022.10.29.0.tar.gz"}

---
+++ zhuoke-kexue-siwei-bixiuke-201803-05/zhuoke-kexue-siwei-bixiuke-201803-05-2022.10.29.0.tar.gz/ZhuokeKexueSiweiBixiuke20180305/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""zhuoke-kexue-siwei-bixiuke-201803-05
+https://github.com/apachecn/zhuoke-kexue-siwei-bixiuke-201803-05"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.29.0"
