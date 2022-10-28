751dc003a17892a5983830177d8ab5bca6e9f236
Tom Forbes <tom@tomforb.es>
2022-10-28 17:38:49 +0000

    {"name":"zhangxiaoyu-shangye-jingdian-anli-201802-06","version":"2022.10.29.0","file":"zhangxiaoyu_shangye_jingdian_anli_201802_06-2022.10.29.0-py3-none-any.whl","path":"zhangxiaoyu-shangye-jingdian-anli-201802-06/zhangxiaoyu_shangye_jingdian_anli_201802_06-2022.10.29.0-py3-none-any.whl"}

---
+++ zhangxiaoyu-shangye-jingdian-anli-201802-06/zhangxiaoyu_shangye_jingdian_anli_201802_06-2022.10.29.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""zhangxiaoyu-shangye-jingdian-anli-201802-06
+https://github.com/apachecn/zhangxiaoyu-shangye-jingdian-anli-201802-06"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.29.0"
