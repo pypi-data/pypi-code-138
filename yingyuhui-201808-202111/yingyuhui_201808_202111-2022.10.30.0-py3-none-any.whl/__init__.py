67055fcb99a64249dc473b04efce99e9d6f35b97
Tom Forbes <tom@tomforb.es>
2022-10-29 19:21:53 +0000

    {"name":"yingyuhui-201808-202111","version":"2022.10.30.0","file":"yingyuhui_201808_202111-2022.10.30.0-py3-none-any.whl","path":"yingyuhui-201808-202111/yingyuhui_201808_202111-2022.10.30.0-py3-none-any.whl"}

---
+++ yingyuhui-201808-202111/yingyuhui_201808_202111-2022.10.30.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yingyuhui-201808-202111
+https://github.com/apachecn/yingyuhui-201808-202111"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.30.0"
