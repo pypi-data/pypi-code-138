2a6991620d6e1e4885d5d53365077f22d048f451
Tom Forbes <tom@tomforb.es>
2022-10-29 17:07:18 +0000

    {"name":"bianfu-tingxie-201909-202108","version":"2022.10.30.0","file":"bianfu-tingxie-201909-202108-2022.10.30.0.tar.gz","path":"bianfu-tingxie-201909-202108/bianfu-tingxie-201909-202108-2022.10.30.0.tar.gz"}

---
+++ bianfu-tingxie-201909-202108/bianfu-tingxie-201909-202108-2022.10.30.0.tar.gz/BianfuTingxie201909202108/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""bianfu-tingxie-201909-202108
+https://github.com/apachecn/bianfu-tingxie-201909-202108"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.30.0"
