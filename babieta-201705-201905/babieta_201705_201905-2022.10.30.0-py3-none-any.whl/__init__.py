3fb76619c5584261dcd12bb4ae8872b4eca61653
Tom Forbes <tom@tomforb.es>
2022-10-29 17:05:13 +0000

    {"name":"babieta-201705-201905","version":"2022.10.30.0","file":"babieta_201705_201905-2022.10.30.0-py3-none-any.whl","path":"babieta-201705-201905/babieta_201705_201905-2022.10.30.0-py3-none-any.whl"}

---
+++ babieta-201705-201905/babieta_201705_201905-2022.10.30.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""babieta-201705-201905
+https://github.com/apachecn/babieta-201705-201905"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.30.0"
