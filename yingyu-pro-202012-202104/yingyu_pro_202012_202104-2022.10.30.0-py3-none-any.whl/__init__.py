f701792777ae1c6eb002f5702d07bb888404c953
Tom Forbes <tom@tomforb.es>
2022-10-29 19:19:24 +0000

    {"name":"yingyu-pro-202012-202104","version":"2022.10.30.0","file":"yingyu_pro_202012_202104-2022.10.30.0-py3-none-any.whl","path":"yingyu-pro-202012-202104/yingyu_pro_202012_202104-2022.10.30.0-py3-none-any.whl"}

---
+++ yingyu-pro-202012-202104/yingyu_pro_202012_202104-2022.10.30.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yingyu-pro-202012-202104
+https://github.com/apachecn/yingyu-pro-202012-202104"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.30.0"
