5c57e2fcc26387a7043c9af5dd6a9cc76f1b4b14
Tom Forbes <tom@tomforb.es>
2022-10-29 19:19:18 +0000

    {"name":"yingyu-pro-201905-202011","version":"2022.10.30.0","file":"yingyu-pro-201905-202011-2022.10.30.0.tar.gz","path":"yingyu-pro-201905-202011/yingyu-pro-201905-202011-2022.10.30.0.tar.gz"}

---
+++ yingyu-pro-201905-202011/yingyu-pro-201905-202011-2022.10.30.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import YingyuPro201905202011
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('YingyuPro201905202011'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="yingyu-pro-201905-202011",
+    version=YingyuPro201905202011.__version__,
+    url="https://github.com/apachecn/yingyu-pro-201905-202011",
+    author=YingyuPro201905202011.__author__,
+    author_email=YingyuPro201905202011.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="英语Pro 2019.5~2020.11",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "yingyu-pro-201905-202011=YingyuPro201905202011.__main__:main",
+            "YingyuPro201905202011=YingyuPro201905202011.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
