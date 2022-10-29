ac449377092a46e63fa56f0b12dfdd633ca33f0c
Tom Forbes <tom@tomforb.es>
2022-10-29 19:00:21 +0000

    {"name":"sixth-tone-202201-202205","version":"2022.10.30.0","file":"sixth-tone-202201-202205-2022.10.30.0.tar.gz","path":"sixth-tone-202201-202205/sixth-tone-202201-202205-2022.10.30.0.tar.gz"}

---
+++ sixth-tone-202201-202205/sixth-tone-202201-202205-2022.10.30.0.tar.gz/SixthTone202201202205/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""sixth-tone-202201-202205
+https://github.com/apachecn/sixth-tone-202201-202205"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.30.0"
