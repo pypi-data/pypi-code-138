d8b489cf0a5ac725fb306699d00b3643a18276e6
Tom Forbes <tom@tomforb.es>
2022-10-29 18:17:10 +0000

    {"name":"liuyande-yingyu-tiandi-202003-202210","version":"2022.10.30.0","file":"liuyande_yingyu_tiandi_202003_202210-2022.10.30.0-py3-none-any.whl","path":"liuyande-yingyu-tiandi-202003-202210/liuyande_yingyu_tiandi_202003_202210-2022.10.30.0-py3-none-any.whl"}

---
+++ liuyande-yingyu-tiandi-202003-202210/liuyande_yingyu_tiandi_202003_202210-2022.10.30.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""liuyande-yingyu-tiandi-202003-202210
+https://github.com/apachecn/liuyande-yingyu-tiandi-202003-202210"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.30.0"
