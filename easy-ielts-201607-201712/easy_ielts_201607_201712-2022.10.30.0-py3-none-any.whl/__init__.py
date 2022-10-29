f3327dd03cbb0488f9735a13452cb161a12ee083
Tom Forbes <tom@tomforb.es>
2022-10-29 18:12:01 +0000

    {"name":"easy-ielts-201607-201712","version":"2022.10.30.0","file":"easy_ielts_201607_201712-2022.10.30.0-py3-none-any.whl","path":"easy-ielts-201607-201712/easy_ielts_201607_201712-2022.10.30.0-py3-none-any.whl"}

---
+++ easy-ielts-201607-201712/easy_ielts_201607_201712-2022.10.30.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""easy-ielts-201607-201712
+https://github.com/apachecn/easy-ielts-201607-201712"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.30.0"
