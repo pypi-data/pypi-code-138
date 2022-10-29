e3db58fadf6e7a0764c5152b3f69871283460c87
Tom Forbes <tom@tomforb.es>
2022-10-29 18:27:37 +0000

    {"name":"yiwan-danci-201608-201811","version":"2022.10.30.0","file":"yiwan-danci-201608-201811-2022.10.30.0.tar.gz","path":"yiwan-danci-201608-201811/yiwan-danci-201608-201811-2022.10.30.0.tar.gz"}

---
+++ yiwan-danci-201608-201811/yiwan-danci-201608-201811-2022.10.30.0.tar.gz/YiwanDanci201608201811/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yiwan-danci-201608-201811
+https://github.com/apachecn/yiwan-danci-201608-201811"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.30.0"
