7fe0ac27e3165dcb07410ec212ba018ca8e08d12
Tom Forbes <tom@tomforb.es>
2022-10-29 19:29:10 +0000

    {"name":"yuyanxue-wucan-201601-202022","version":"2022.10.30.0","file":"yuyanxue_wucan_201601_202022-2022.10.30.0-py3-none-any.whl","path":"yuyanxue-wucan-201601-202022/yuyanxue_wucan_201601_202022-2022.10.30.0-py3-none-any.whl"}

---
+++ yuyanxue-wucan-201601-202022/yuyanxue_wucan_201601_202022-2022.10.30.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yuyanxue-wucan-201601-202022
+https://github.com/apachecn/yuyanxue-wucan-201601-202022"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.30.0"
