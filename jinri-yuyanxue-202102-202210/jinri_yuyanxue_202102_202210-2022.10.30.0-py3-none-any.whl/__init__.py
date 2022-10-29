510a6eb0a6be375cac3bddc49b841ead505b8e56
Tom Forbes <tom@tomforb.es>
2022-10-29 18:16:23 +0000

    {"name":"jinri-yuyanxue-202102-202210","version":"2022.10.30.0","file":"jinri_yuyanxue_202102_202210-2022.10.30.0-py3-none-any.whl","path":"jinri-yuyanxue-202102-202210/jinri_yuyanxue_202102_202210-2022.10.30.0-py3-none-any.whl"}

---
+++ jinri-yuyanxue-202102-202210/jinri_yuyanxue_202102_202210-2022.10.30.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jinri-yuyanxue-202102-202210
+https://github.com/apachecn/jinri-yuyanxue-202102-202210"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.30.0"
