81abe69236ee5787a7d74e04c3cf27cf9b8963c1
Tom Forbes <tom@tomforb.es>
2022-10-29 17:56:06 +0000

    {"name":"babieta-201312-201511","version":"2022.10.30.0","file":"babieta-201312-201511-2022.10.30.0.tar.gz","path":"babieta-201312-201511/babieta-201312-201511-2022.10.30.0.tar.gz"}

---
+++ babieta-201312-201511/babieta-201312-201511-2022.10.30.0.tar.gz/Babieta201312201511/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""babieta-201312-201511
+https://github.com/apachecn/babieta-201312-201511"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.30.0"
---
+++ babieta-201312-201511/babieta-201312-201511-2022.10.30.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import Babieta201312201511
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('Babieta201312201511'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="babieta-201312-201511",
+    version=Babieta201312201511.__version__,
+    url="https://github.com/apachecn/babieta-201312-201511",
+    author=Babieta201312201511.__author__,
+    author_email=Babieta201312201511.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="巴别塔 2013.12~2015.11",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "babieta-201312-201511=Babieta201312201511.__main__:main",
+            "Babieta201312201511=Babieta201312201511.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
