e53ba5f507da81a39691de338a2f19efd075c70f
Tom Forbes <tom@tomforb.es>
2022-10-29 17:33:04 +0000

    {"name":"yingyu-kouyu-201912","version":"2022.10.30.0","file":"yingyu-kouyu-201912-2022.10.30.0.tar.gz","path":"yingyu-kouyu-201912/yingyu-kouyu-201912-2022.10.30.0.tar.gz"}

---
+++ yingyu-kouyu-201912/yingyu-kouyu-201912-2022.10.30.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import YingyuKouyu201912
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('YingyuKouyu201912'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="yingyu-kouyu-201912",
+    version=YingyuKouyu201912.__version__,
+    url="https://github.com/apachecn/yingyu-kouyu-201912",
+    author=YingyuKouyu201912.__author__,
+    author_email=YingyuKouyu201912.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="英语口语 2019.12",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "yingyu-kouyu-201912=YingyuKouyu201912.__main__:main",
+            "YingyuKouyu201912=YingyuKouyu201912.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
