38c5664f53ed39716846bb21c6c603e93624d3c9
Tom Forbes <tom@tomforb.es>
2022-10-29 17:57:36 +0000

    {"name":"catti-kaoshi-ziliao-yuzixun-201704-201712","version":"2022.10.30.0","file":"catti_kaoshi_ziliao_yuzixun_201704_201712-2022.10.30.0-py3-none-any.whl","path":"catti-kaoshi-ziliao-yuzixun-201704-201712/catti_kaoshi_ziliao_yuzixun_201704_201712-2022.10.30.0-py3-none-any.whl"}

---
+++ catti-kaoshi-ziliao-yuzixun-201704-201712/catti_kaoshi_ziliao_yuzixun_201704_201712-2022.10.30.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""catti-kaoshi-ziliao-yuzixun-201704-201712
+https://github.com/apachecn/catti-kaoshi-ziliao-yuzixun-201704-201712"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.30.0"
