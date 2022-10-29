0390766f4f246af530b367861845a3a946230633
Tom Forbes <tom@tomforb.es>
2022-10-29 17:25:21 +0000

    {"name":"kaoyan-yingyu-huangpishu-201808-201902","version":"2022.10.30.0","file":"kaoyan-yingyu-huangpishu-201808-201902-2022.10.30.0.tar.gz","path":"kaoyan-yingyu-huangpishu-201808-201902/kaoyan-yingyu-huangpishu-201808-201902-2022.10.30.0.tar.gz"}

---
+++ kaoyan-yingyu-huangpishu-201808-201902/kaoyan-yingyu-huangpishu-201808-201902-2022.10.30.0.tar.gz/KaoyanYingyuHuangpishu201808201902/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""kaoyan-yingyu-huangpishu-201808-201902
+https://github.com/apachecn/kaoyan-yingyu-huangpishu-201808-201902"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.30.0"
