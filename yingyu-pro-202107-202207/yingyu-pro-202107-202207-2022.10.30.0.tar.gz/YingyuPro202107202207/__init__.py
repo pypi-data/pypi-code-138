b8c3989e88dbe1bd7db37a1744ba5c84fc0febaf
Tom Forbes <tom@tomforb.es>
2022-10-29 19:19:34 +0000

    {"name":"yingyu-pro-202107-202207","version":"2022.10.30.0","file":"yingyu-pro-202107-202207-2022.10.30.0.tar.gz","path":"yingyu-pro-202107-202207/yingyu-pro-202107-202207-2022.10.30.0.tar.gz"}

---
+++ yingyu-pro-202107-202207/yingyu-pro-202107-202207-2022.10.30.0.tar.gz/YingyuPro202107202207/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yingyu-pro-202107-202207
+https://github.com/apachecn/yingyu-pro-202107-202207"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.30.0"
