523c451b220ed5a536050acff134c92d65a7a542
Tom Forbes <tom@tomforb.es>
2022-10-29 18:40:26 +0000

    {"name":"sixth-tone-201711-201802","version":"2022.10.30.0","file":"sixth-tone-201711-201802-2022.10.30.0.tar.gz","path":"sixth-tone-201711-201802/sixth-tone-201711-201802-2022.10.30.0.tar.gz"}

---
+++ sixth-tone-201711-201802/sixth-tone-201711-201802-2022.10.30.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import SixthTone201711201802
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('SixthTone201711201802'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="sixth-tone-201711-201802",
+    version=SixthTone201711201802.__version__,
+    url="https://github.com/apachecn/sixth-tone-201711-201802",
+    author=SixthTone201711201802.__author__,
+    author_email=SixthTone201711201802.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="SixthTone 2017.11~2018.2",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "sixth-tone-201711-201802=SixthTone201711201802.__main__:main",
+            "SixthTone201711201802=SixthTone201711201802.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
