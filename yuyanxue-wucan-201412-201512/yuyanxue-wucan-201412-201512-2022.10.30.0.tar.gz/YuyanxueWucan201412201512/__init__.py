73635666e920c4e548b0bcdd6f48d3c9d8ab3f01
Tom Forbes <tom@tomforb.es>
2022-10-29 19:28:29 +0000

    {"name":"yuyanxue-wucan-201412-201512","version":"2022.10.30.0","file":"yuyanxue-wucan-201412-201512-2022.10.30.0.tar.gz","path":"yuyanxue-wucan-201412-201512/yuyanxue-wucan-201412-201512-2022.10.30.0.tar.gz"}

---
+++ yuyanxue-wucan-201412-201512/yuyanxue-wucan-201412-201512-2022.10.30.0.tar.gz/YuyanxueWucan201412201512/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yuyanxue-wucan-201412-201512
+https://github.com/apachecn/yuyanxue-wucan-201412-201512"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.30.0"
