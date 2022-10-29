350e936955f1826be4f53e2717e1db72bd338ff3
Tom Forbes <tom@tomforb.es>
2022-10-29 19:27:17 +0000

    {"name":"yuyan-zhanlue-yanjiu-201903-202210","version":"2022.10.30.0","file":"yuyan_zhanlue_yanjiu_201903_202210-2022.10.30.0-py3-none-any.whl","path":"yuyan-zhanlue-yanjiu-201903-202210/yuyan_zhanlue_yanjiu_201903_202210-2022.10.30.0-py3-none-any.whl"}

---
+++ yuyan-zhanlue-yanjiu-201903-202210/yuyan_zhanlue_yanjiu_201903_202210-2022.10.30.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yuyan-zhanlue-yanjiu-201903-202210
+https://github.com/apachecn/yuyan-zhanlue-yanjiu-201903-202210"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.30.0"
