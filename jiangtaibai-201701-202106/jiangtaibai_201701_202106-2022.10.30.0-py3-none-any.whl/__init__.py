97d65361929100b8873be1527a1ac2aa9f7cf36e
Tom Forbes <tom@tomforb.es>
2022-10-29 17:21:04 +0000

    {"name":"jiangtaibai-201701-202106","version":"2022.10.30.0","file":"jiangtaibai_201701_202106-2022.10.30.0-py3-none-any.whl","path":"jiangtaibai-201701-202106/jiangtaibai_201701_202106-2022.10.30.0-py3-none-any.whl"}

---
+++ jiangtaibai-201701-202106/jiangtaibai_201701_202106-2022.10.30.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jiangtaibai-201701-202106
+https://github.com/apachecn/jiangtaibai-201701-202106"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.30.0"
