b4414a9c04d72db461e07a40895e9d7a30a81be4
Tom Forbes <tom@tomforb.es>
2022-10-29 19:18:47 +0000

    {"name":"yijia-201801-202012","version":"2022.10.30.0","file":"yijia_201801_202012-2022.10.30.0-py3-none-any.whl","path":"yijia-201801-202012/yijia_201801_202012-2022.10.30.0-py3-none-any.whl"}

---
+++ yijia-201801-202012/yijia_201801_202012-2022.10.30.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yijia-201801-202012
+https://github.com/apachecn/yijia-201801-202012"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.30.0"
