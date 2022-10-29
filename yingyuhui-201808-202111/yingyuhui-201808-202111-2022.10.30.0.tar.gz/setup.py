e6197123a365155c0c84d97566113d699237d2d8
Tom Forbes <tom@tomforb.es>
2022-10-29 19:22:07 +0000

    {"name":"yingyuhui-201808-202111","version":"2022.10.30.0","file":"yingyuhui-201808-202111-2022.10.30.0.tar.gz","path":"yingyuhui-201808-202111/yingyuhui-201808-202111-2022.10.30.0.tar.gz"}

---
+++ yingyuhui-201808-202111/yingyuhui-201808-202111-2022.10.30.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import Yingyuhui201808202111
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('Yingyuhui201808202111'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="yingyuhui-201808-202111",
+    version=Yingyuhui201808202111.__version__,
+    url="https://github.com/apachecn/yingyuhui-201808-202111",
+    author=Yingyuhui201808202111.__author__,
+    author_email=Yingyuhui201808202111.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="英语汇 2018.8~2021.11",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "yingyuhui-201808-202111=Yingyuhui201808202111.__main__:main",
+            "Yingyuhui201808202111=Yingyuhui201808202111.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
