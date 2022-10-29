f5442f8ebe36aab840d9df94ec78faf56c47e695
Tom Forbes <tom@tomforb.es>
2022-10-29 17:07:27 +0000

    {"name":"bingo-liaoyingyu-201301-201605","version":"2022.10.30.0","file":"bingo_liaoyingyu_201301_201605-2022.10.30.0-py3-none-any.whl","path":"bingo-liaoyingyu-201301-201605/bingo_liaoyingyu_201301_201605-2022.10.30.0-py3-none-any.whl"}

---
+++ bingo-liaoyingyu-201301-201605/bingo_liaoyingyu_201301_201605-2022.10.30.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""bingo-liaoyingyu-201301-201605
+https://github.com/apachecn/bingo-liaoyingyu-201301-201605"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.30.0"
