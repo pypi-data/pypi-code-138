fa534f70c5a74126ba92e4de52899552cf78bc59
Tom Forbes <tom@tomforb.es>
2022-10-29 17:29:22 +0000

    {"name":"renren-doushi-fanyiguan-201701-202104","version":"2022.10.30.0","file":"renren-doushi-fanyiguan-201701-202104-2022.10.30.0.tar.gz","path":"renren-doushi-fanyiguan-201701-202104/renren-doushi-fanyiguan-201701-202104-2022.10.30.0.tar.gz"}

---
+++ renren-doushi-fanyiguan-201701-202104/renren-doushi-fanyiguan-201701-202104-2022.10.30.0.tar.gz/RenrenDoushiFanyiguan201701202104/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""renren-doushi-fanyiguan-201701-202104
+https://github.com/apachecn/renren-doushi-fanyiguan-201701-202104"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.30.0"
