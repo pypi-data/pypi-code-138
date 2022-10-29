8e77b85fa1d1c2686f9ace4d19473af92faf30c4
Tom Forbes <tom@tomforb.es>
2022-10-29 17:10:20 +0000

    {"name":"gaozhai-catti-201502-201605","version":"2022.10.30.0","file":"gaozhai_catti_201502_201605-2022.10.30.0-py3-none-any.whl","path":"gaozhai-catti-201502-201605/gaozhai_catti_201502_201605-2022.10.30.0-py3-none-any.whl"}

---
+++ gaozhai-catti-201502-201605/gaozhai_catti_201502_201605-2022.10.30.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""gaozhai-catti-201502-201605
+https://github.com/apachecn/gaozhai-catti-201502-201605"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.30.0"
