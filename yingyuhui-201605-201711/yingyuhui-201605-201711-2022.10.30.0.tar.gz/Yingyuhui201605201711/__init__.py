bc22968a99a65feecbc4d7fdd7eab29266da40c4
Tom Forbes <tom@tomforb.es>
2022-10-29 19:20:42 +0000

    {"name":"yingyuhui-201605-201711","version":"2022.10.30.0","file":"yingyuhui-201605-201711-2022.10.30.0.tar.gz","path":"yingyuhui-201605-201711/yingyuhui-201605-201711-2022.10.30.0.tar.gz"}

---
+++ yingyuhui-201605-201711/yingyuhui-201605-201711-2022.10.30.0.tar.gz/Yingyuhui201605201711/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yingyuhui-201605-201711
+https://github.com/apachecn/yingyuhui-201605-201711"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.30.0"
