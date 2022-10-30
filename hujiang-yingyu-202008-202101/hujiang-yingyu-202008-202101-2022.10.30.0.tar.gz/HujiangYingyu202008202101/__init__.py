1d74c2d270889b4c04d4cd7b16b09158b2c0cefe
Tom Forbes <tom@tomforb.es>
2022-10-30 04:43:12 +0000

    {"name":"hujiang-yingyu-202008-202101","version":"2022.10.30.0","file":"hujiang-yingyu-202008-202101-2022.10.30.0.tar.gz","path":"hujiang-yingyu-202008-202101/hujiang-yingyu-202008-202101-2022.10.30.0.tar.gz"}

---
+++ hujiang-yingyu-202008-202101/hujiang-yingyu-202008-202101-2022.10.30.0.tar.gz/HujiangYingyu202008202101/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""hujiang-yingyu-202008-202101
+https://github.com/apachecn/hujiang-yingyu-202008-202101"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.30.0"
