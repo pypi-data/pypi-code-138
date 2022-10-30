e1a71f5c276897d1d4a8cccf62c1902f1f7028e2
Tom Forbes <tom@tomforb.es>
2022-10-30 05:00:25 +0000

    {"name":"yingyu-kouyu-xiaozhen-201802-201806","version":"2022.10.30.0","file":"yingyu-kouyu-xiaozhen-201802-201806-2022.10.30.0.tar.gz","path":"yingyu-kouyu-xiaozhen-201802-201806/yingyu-kouyu-xiaozhen-201802-201806-2022.10.30.0.tar.gz"}

---
+++ yingyu-kouyu-xiaozhen-201802-201806/yingyu-kouyu-xiaozhen-201802-201806-2022.10.30.0.tar.gz/YingyuKouyuXiaozhen201802201806/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yingyu-kouyu-xiaozhen-201802-201806
+https://github.com/apachecn/yingyu-kouyu-xiaozhen-201802-201806"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.30.0"
