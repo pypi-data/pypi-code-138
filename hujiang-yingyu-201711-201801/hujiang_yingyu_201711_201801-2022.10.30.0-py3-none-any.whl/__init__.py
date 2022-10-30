fc772c5b3721dff065bd85683f8b07e5af077f72
Tom Forbes <tom@tomforb.es>
2022-10-30 04:36:16 +0000

    {"name":"hujiang-yingyu-201711-201801","version":"2022.10.30.0","file":"hujiang_yingyu_201711_201801-2022.10.30.0-py3-none-any.whl","path":"hujiang-yingyu-201711-201801/hujiang_yingyu_201711_201801-2022.10.30.0-py3-none-any.whl"}

---
+++ hujiang-yingyu-201711-201801/hujiang_yingyu_201711_201801-2022.10.30.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""hujiang-yingyu-201711-201801
+https://github.com/apachecn/hujiang-yingyu-201711-201801"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.30.0"
