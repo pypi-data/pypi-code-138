4f457efaf5153a8864e2f704ceb5c7ae859070f1
Tom Forbes <tom@tomforb.es>
2022-10-30 05:28:43 +0000

    {"name":"yuyanxue-tongxun-202205-202210","version":"2022.10.30.0","file":"yuyanxue-tongxun-202205-202210-2022.10.30.0.tar.gz","path":"yuyanxue-tongxun-202205-202210/yuyanxue-tongxun-202205-202210-2022.10.30.0.tar.gz"}

---
+++ yuyanxue-tongxun-202205-202210/yuyanxue-tongxun-202205-202210-2022.10.30.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import YuyanxueTongxun202205202210
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('YuyanxueTongxun202205202210'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="yuyanxue-tongxun-202205-202210",
+    version=YuyanxueTongxun202205202210.__version__,
+    url="https://github.com/apachecn/yuyanxue-tongxun-202205-202210",
+    author=YuyanxueTongxun202205202210.__author__,
+    author_email=YuyanxueTongxun202205202210.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="语言学通讯 2022.5~2022.10",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "yuyanxue-tongxun-202205-202210=YuyanxueTongxun202205202210.__main__:main",
+            "YuyanxueTongxun202205202210=YuyanxueTongxun202205202210.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
