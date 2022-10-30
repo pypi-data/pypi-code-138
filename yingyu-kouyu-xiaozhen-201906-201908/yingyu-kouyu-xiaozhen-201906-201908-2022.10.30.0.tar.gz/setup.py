172e0a949308f14969ac2e32a4ebfd04d52c2149
Tom Forbes <tom@tomforb.es>
2022-10-30 05:02:19 +0000

    {"name":"yingyu-kouyu-xiaozhen-201906-201908","version":"2022.10.30.0","file":"yingyu-kouyu-xiaozhen-201906-201908-2022.10.30.0.tar.gz","path":"yingyu-kouyu-xiaozhen-201906-201908/yingyu-kouyu-xiaozhen-201906-201908-2022.10.30.0.tar.gz"}

---
+++ yingyu-kouyu-xiaozhen-201906-201908/yingyu-kouyu-xiaozhen-201906-201908-2022.10.30.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import YingyuKouyuXiaozhen201906201908
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('YingyuKouyuXiaozhen201906201908'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="yingyu-kouyu-xiaozhen-201906-201908",
+    version=YingyuKouyuXiaozhen201906201908.__version__,
+    url="https://github.com/apachecn/yingyu-kouyu-xiaozhen-201906-201908",
+    author=YingyuKouyuXiaozhen201906201908.__author__,
+    author_email=YingyuKouyuXiaozhen201906201908.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="英语口语小镇 2019.6~2019.8",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "yingyu-kouyu-xiaozhen-201906-201908=YingyuKouyuXiaozhen201906201908.__main__:main",
+            "YingyuKouyuXiaozhen201906201908=YingyuKouyuXiaozhen201906201908.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
