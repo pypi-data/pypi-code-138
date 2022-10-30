e59df13bdbc34987131c7f5ce0b48b1a36f4338a
Tom Forbes <tom@tomforb.es>
2022-10-30 15:05:56 +0000

    {"name":"apachecn-quant-zh","version":"2022.10.30.0","file":"apachecn-quant-zh-2022.10.30.0.tar.gz","path":"apachecn-quant-zh/apachecn-quant-zh-2022.10.30.0.tar.gz"}

---
+++ apachecn-quant-zh/apachecn-quant-zh-2022.10.30.0.tar.gz/ApachecnQuantZh/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""apachecn-quant-zh
+https://github.com/apachecn/apachecn-quant-zh"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.30.0"
