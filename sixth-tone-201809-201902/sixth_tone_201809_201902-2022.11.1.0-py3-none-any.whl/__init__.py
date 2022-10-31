40acc0d2147899bb3da64281f8b35d19a8f14545
Tom Forbes <tom@tomforb.es>
2022-10-31 18:19:35 +0000

    {"name":"sixth-tone-201809-201902","version":"2022.11.1.0","file":"sixth_tone_201809_201902-2022.11.1.0-py3-none-any.whl","path":"sixth-tone-201809-201902/sixth_tone_201809_201902-2022.11.1.0-py3-none-any.whl"}

---
+++ sixth-tone-201809-201902/sixth_tone_201809_201902-2022.11.1.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""sixth-tone-201809-201902
+https://github.com/apachecn/sixth-tone-201809-201902"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.1.0"
