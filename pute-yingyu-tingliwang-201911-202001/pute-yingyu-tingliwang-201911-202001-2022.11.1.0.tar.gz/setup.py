74e616c6fa619f0ecf4f09b5bae089418e3658c9
Tom Forbes <tom@tomforb.es>
2022-10-31 18:15:00 +0000

    {"name":"pute-yingyu-tingliwang-201911-202001","version":"2022.11.1.0","file":"pute-yingyu-tingliwang-201911-202001-2022.11.1.0.tar.gz","path":"pute-yingyu-tingliwang-201911-202001/pute-yingyu-tingliwang-201911-202001-2022.11.1.0.tar.gz"}

---
+++ pute-yingyu-tingliwang-201911-202001/pute-yingyu-tingliwang-201911-202001-2022.11.1.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import PuteYingyuTingliwang201911202001
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('PuteYingyuTingliwang201911202001'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="pute-yingyu-tingliwang-201911-202001",
+    version=PuteYingyuTingliwang201911202001.__version__,
+    url="https://github.com/apachecn/pute-yingyu-tingliwang-201911-202001",
+    author=PuteYingyuTingliwang201911202001.__author__,
+    author_email=PuteYingyuTingliwang201911202001.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="普特英语听力网 2019.11~2020.1",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "pute-yingyu-tingliwang-201911-202001=PuteYingyuTingliwang201911202001.__main__:main",
+            "PuteYingyuTingliwang201911202001=PuteYingyuTingliwang201911202001.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
