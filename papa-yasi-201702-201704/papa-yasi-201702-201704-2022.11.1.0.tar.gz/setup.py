83cc20b9ecd67fa80f94fb6ef3caacda500f1fae
Tom Forbes <tom@tomforb.es>
2022-10-31 17:52:43 +0000

    {"name":"papa-yasi-201702-201704","version":"2022.11.1.0","file":"papa-yasi-201702-201704-2022.11.1.0.tar.gz","path":"papa-yasi-201702-201704/papa-yasi-201702-201704-2022.11.1.0.tar.gz"}

---
+++ papa-yasi-201702-201704/papa-yasi-201702-201704-2022.11.1.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import PapaYasi201702201704
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('PapaYasi201702201704'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="papa-yasi-201702-201704",
+    version=PapaYasi201702201704.__version__,
+    url="https://github.com/apachecn/papa-yasi-201702-201704",
+    author=PapaYasi201702201704.__author__,
+    author_email=PapaYasi201702201704.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="趴趴雅思 2017.2~2017.4",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "papa-yasi-201702-201704=PapaYasi201702201704.__main__:main",
+            "PapaYasi201702201704=PapaYasi201702201704.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
