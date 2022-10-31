3d688b74341100b2b76d70d761dd8d284b67f10d
Tom Forbes <tom@tomforb.es>
2022-10-31 18:48:07 +0000

    {"name":"yijiang-202006-202008","version":"2022.11.1.0","file":"yijiang-202006-202008-2022.11.1.0.tar.gz","path":"yijiang-202006-202008/yijiang-202006-202008-2022.11.1.0.tar.gz"}

---
+++ yijiang-202006-202008/yijiang-202006-202008-2022.11.1.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import Yijiang202006202008
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('Yijiang202006202008'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="yijiang-202006-202008",
+    version=Yijiang202006202008.__version__,
+    url="https://github.com/apachecn/yijiang-202006-202008",
+    author=Yijiang202006202008.__author__,
+    author_email=Yijiang202006202008.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="译匠 2020.6~2020.8",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "yijiang-202006-202008=Yijiang202006202008.__main__:main",
+            "Yijiang202006202008=Yijiang202006202008.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
