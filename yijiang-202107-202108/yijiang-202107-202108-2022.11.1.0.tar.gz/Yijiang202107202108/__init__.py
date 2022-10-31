a7eb18398581a4ef0cd3294f70d1b112a07df352
Tom Forbes <tom@tomforb.es>
2022-10-31 18:50:32 +0000

    {"name":"yijiang-202107-202108","version":"2022.11.1.0","file":"yijiang-202107-202108-2022.11.1.0.tar.gz","path":"yijiang-202107-202108/yijiang-202107-202108-2022.11.1.0.tar.gz"}

---
+++ yijiang-202107-202108/yijiang-202107-202108-2022.11.1.0.tar.gz/Yijiang202107202108/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yijiang-202107-202108
+https://github.com/apachecn/yijiang-202107-202108"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.1.0"
