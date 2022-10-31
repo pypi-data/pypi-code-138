0d37b263244aa8b9eadf054ec60e7303c7453b98
Tom Forbes <tom@tomforb.es>
2022-10-31 18:34:33 +0000

    {"name":"xianhua-yingyu-202008-202012","version":"2022.11.1.0","file":"xianhua_yingyu_202008_202012-2022.11.1.0-py3-none-any.whl","path":"xianhua-yingyu-202008-202012/xianhua_yingyu_202008_202012-2022.11.1.0-py3-none-any.whl"}

---
+++ xianhua-yingyu-202008-202012/xianhua_yingyu_202008_202012-2022.11.1.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""xianhua-yingyu-202008-202012
+https://github.com/apachecn/xianhua-yingyu-202008-202012"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.1.0"
