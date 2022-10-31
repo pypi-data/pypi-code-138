dafeeb62d72b4c423e12aeb0e52b02d4913f6ef2
Tom Forbes <tom@tomforb.es>
2022-10-31 18:28:57 +0000

    {"name":"xianhua-yingyu-201908-202001","version":"2022.11.1.0","file":"xianhua-yingyu-201908-202001-2022.11.1.0.tar.gz","path":"xianhua-yingyu-201908-202001/xianhua-yingyu-201908-202001-2022.11.1.0.tar.gz"}

---
+++ xianhua-yingyu-201908-202001/xianhua-yingyu-201908-202001-2022.11.1.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import XianhuaYingyu201908202001
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('XianhuaYingyu201908202001'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="xianhua-yingyu-201908-202001",
+    version=XianhuaYingyu201908202001.__version__,
+    url="https://github.com/apachecn/xianhua-yingyu-201908-202001",
+    author=XianhuaYingyu201908202001.__author__,
+    author_email=XianhuaYingyu201908202001.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="线话英语 2019.8~2020.1",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "xianhua-yingyu-201908-202001=XianhuaYingyu201908202001.__main__:main",
+            "XianhuaYingyu201908202001=XianhuaYingyu201908202001.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
