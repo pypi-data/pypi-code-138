1781a1cc2a97bcdf7bb0d50fbdb2ea223140fd87
Tom Forbes <tom@tomforb.es>
2022-10-31 18:17:33 +0000

    {"name":"pute-yingyu-tingliwang-202108-202110","version":"2022.11.1.0","file":"pute-yingyu-tingliwang-202108-202110-2022.11.1.0.tar.gz","path":"pute-yingyu-tingliwang-202108-202110/pute-yingyu-tingliwang-202108-202110-2022.11.1.0.tar.gz"}

---
+++ pute-yingyu-tingliwang-202108-202110/pute-yingyu-tingliwang-202108-202110-2022.11.1.0.tar.gz/PuteYingyuTingliwang202108202110/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""pute-yingyu-tingliwang-202108-202110
+https://github.com/apachecn/pute-yingyu-tingliwang-202108-202110"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.1.0"
