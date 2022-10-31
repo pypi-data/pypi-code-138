01aec8b0a31a90d6990c3c8ddc0c90ce66f40c27
Tom Forbes <tom@tomforb.es>
2022-10-31 18:47:28 +0000

    {"name":"yijiang-202002-202005","version":"2022.11.1.0","file":"yijiang_202002_202005-2022.11.1.0-py3-none-any.whl","path":"yijiang-202002-202005/yijiang_202002_202005-2022.11.1.0-py3-none-any.whl"}

---
+++ yijiang-202002-202005/yijiang_202002_202005-2022.11.1.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yijiang-202002-202005
+https://github.com/apachecn/yijiang-202002-202005"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.1.0"
