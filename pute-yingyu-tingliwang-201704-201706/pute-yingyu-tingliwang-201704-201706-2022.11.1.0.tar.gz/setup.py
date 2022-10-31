f4c5ca5ee819d447277bf4f940e81fd1f2b6bf00
Tom Forbes <tom@tomforb.es>
2022-10-31 18:10:14 +0000

    {"name":"pute-yingyu-tingliwang-201704-201706","version":"2022.11.1.0","file":"pute-yingyu-tingliwang-201704-201706-2022.11.1.0.tar.gz","path":"pute-yingyu-tingliwang-201704-201706/pute-yingyu-tingliwang-201704-201706-2022.11.1.0.tar.gz"}

---
+++ pute-yingyu-tingliwang-201704-201706/pute-yingyu-tingliwang-201704-201706-2022.11.1.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import PuteYingyuTingliwang201704201706
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('PuteYingyuTingliwang201704201706'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="pute-yingyu-tingliwang-201704-201706",
+    version=PuteYingyuTingliwang201704201706.__version__,
+    url="https://github.com/apachecn/pute-yingyu-tingliwang-201704-201706",
+    author=PuteYingyuTingliwang201704201706.__author__,
+    author_email=PuteYingyuTingliwang201704201706.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="普特英语听力网 2017.4~2017.6",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "pute-yingyu-tingliwang-201704-201706=PuteYingyuTingliwang201704201706.__main__:main",
+            "PuteYingyuTingliwang201704201706=PuteYingyuTingliwang201704201706.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
