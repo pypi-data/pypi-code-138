e57b44fd6e21ada1a34e8f4f3a716b66311746f7
Tom Forbes <tom@tomforb.es>
2022-11-01 11:22:37 +0000

    {"name":"beijing-lvyou-riyu-202205-202210","version":"2022.11.1.0","file":"beijing-lvyou-riyu-202205-202210-2022.11.1.0.tar.gz","path":"beijing-lvyou-riyu-202205-202210/beijing-lvyou-riyu-202205-202210-2022.11.1.0.tar.gz"}

---
+++ beijing-lvyou-riyu-202205-202210/beijing-lvyou-riyu-202205-202210-2022.11.1.0.tar.gz/BeijingLvyouRiyu202205202210/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""beijing-lvyou-riyu-202205-202210
+https://github.com/apachecn/beijing-lvyou-riyu-202205-202210"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.1.0"
