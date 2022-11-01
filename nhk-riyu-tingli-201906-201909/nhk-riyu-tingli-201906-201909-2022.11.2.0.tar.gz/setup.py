d1d540a0d324e063f3f8d754d5ddcf22c2049dcb
Tom Forbes <tom@tomforb.es>
2022-11-01 19:05:29 +0000

    {"name":"nhk-riyu-tingli-201906-201909","version":"2022.11.2.0","file":"nhk-riyu-tingli-201906-201909-2022.11.2.0.tar.gz","path":"nhk-riyu-tingli-201906-201909/nhk-riyu-tingli-201906-201909-2022.11.2.0.tar.gz"}

---
+++ nhk-riyu-tingli-201906-201909/nhk-riyu-tingli-201906-201909-2022.11.2.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import NhkRiyuTingli201906201909
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('NhkRiyuTingli201906201909'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="nhk-riyu-tingli-201906-201909",
+    version=NhkRiyuTingli201906201909.__version__,
+    url="https://github.com/apachecn/nhk-riyu-tingli-201906-201909",
+    author=NhkRiyuTingli201906201909.__author__,
+    author_email=NhkRiyuTingli201906201909.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="NHK日语听力 2019.6~2019.9",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "nhk-riyu-tingli-201906-201909=NhkRiyuTingli201906201909.__main__:main",
+            "NhkRiyuTingli201906201909=NhkRiyuTingli201906201909.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
