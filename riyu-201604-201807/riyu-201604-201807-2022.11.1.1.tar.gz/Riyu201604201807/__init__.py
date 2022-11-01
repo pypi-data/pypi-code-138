0a6c05c13222c44f4453a765c2b7454b10540acc
Tom Forbes <tom@tomforb.es>
2022-11-01 13:21:44 +0000

    {"name":"riyu-201604-201807","version":"2022.11.1.1","file":"riyu-201604-201807-2022.11.1.1.tar.gz","path":"riyu-201604-201807/riyu-201604-201807-2022.11.1.1.tar.gz"}

---
+++ riyu-201604-201807/riyu-201604-201807-2022.11.1.1.tar.gz/Riyu201604201807/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""riyu-201604-201807
+https://github.com/apachecn/riyu-201604-201807"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.1.1"
