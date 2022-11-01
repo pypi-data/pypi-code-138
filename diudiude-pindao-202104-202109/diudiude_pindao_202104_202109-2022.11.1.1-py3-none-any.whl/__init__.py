94683a2f3fe2b1ee7d9f803745412ec4814d9f03
Tom Forbes <tom@tomforb.es>
2022-11-01 12:50:17 +0000

    {"name":"diudiude-pindao-202104-202109","version":"2022.11.1.1","file":"diudiude_pindao_202104_202109-2022.11.1.1-py3-none-any.whl","path":"diudiude-pindao-202104-202109/diudiude_pindao_202104_202109-2022.11.1.1-py3-none-any.whl"}

---
+++ diudiude-pindao-202104-202109/diudiude_pindao_202104_202109-2022.11.1.1-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""diudiude-pindao-202104-202109
+https://github.com/apachecn/diudiude-pindao-202104-202109"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.1.1"
