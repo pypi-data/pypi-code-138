dd042d00d14a5cd2a26e0a87ee0824d066815bf3
Tom Forbes <tom@tomforb.es>
2022-11-01 20:54:31 +0000

    {"name":"zonghe-riyu-201706-201711","version":"2022.11.2.0","file":"zonghe-riyu-201706-201711-2022.11.2.0.tar.gz","path":"zonghe-riyu-201706-201711/zonghe-riyu-201706-201711-2022.11.2.0.tar.gz"}

---
+++ zonghe-riyu-201706-201711/zonghe-riyu-201706-201711-2022.11.2.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import ZongheRiyu201706201711
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('ZongheRiyu201706201711'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="zonghe-riyu-201706-201711",
+    version=ZongheRiyu201706201711.__version__,
+    url="https://github.com/apachecn/zonghe-riyu-201706-201711",
+    author=ZongheRiyu201706201711.__author__,
+    author_email=ZongheRiyu201706201711.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="综合日语 2017.6~2017.11",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "zonghe-riyu-201706-201711=ZongheRiyu201706201711.__main__:main",
+            "ZongheRiyu201706201711=ZongheRiyu201706201711.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
