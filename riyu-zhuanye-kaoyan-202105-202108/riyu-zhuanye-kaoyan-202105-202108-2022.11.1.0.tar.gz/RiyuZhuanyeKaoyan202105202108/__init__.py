5e293968818d93d35dd24c88151ebc74aa2ebae9
Tom Forbes <tom@tomforb.es>
2022-11-01 12:33:16 +0000

    {"name":"riyu-zhuanye-kaoyan-202105-202108","version":"2022.11.1.0","file":"riyu-zhuanye-kaoyan-202105-202108-2022.11.1.0.tar.gz","path":"riyu-zhuanye-kaoyan-202105-202108/riyu-zhuanye-kaoyan-202105-202108-2022.11.1.0.tar.gz"}

---
+++ riyu-zhuanye-kaoyan-202105-202108/riyu-zhuanye-kaoyan-202105-202108-2022.11.1.0.tar.gz/RiyuZhuanyeKaoyan202105202108/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""riyu-zhuanye-kaoyan-202105-202108
+https://github.com/apachecn/riyu-zhuanye-kaoyan-202105-202108"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.1.0"
