937d6375e2d339b5e8e5ab3f18dc9322ad42c2ae
Tom Forbes <tom@tomforb.es>
2022-11-01 20:24:09 +0000

    {"name":"riyu-tingyi-xuexi-202202-202203","version":"2022.11.2.0","file":"riyu-tingyi-xuexi-202202-202203-2022.11.2.0.tar.gz","path":"riyu-tingyi-xuexi-202202-202203/riyu-tingyi-xuexi-202202-202203-2022.11.2.0.tar.gz"}

---
+++ riyu-tingyi-xuexi-202202-202203/riyu-tingyi-xuexi-202202-202203-2022.11.2.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import RiyuTingyiXuexi202202202203
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('RiyuTingyiXuexi202202202203'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="riyu-tingyi-xuexi-202202-202203",
+    version=RiyuTingyiXuexi202202202203.__version__,
+    url="https://github.com/apachecn/riyu-tingyi-xuexi-202202-202203",
+    author=RiyuTingyiXuexi202202202203.__author__,
+    author_email=RiyuTingyiXuexi202202202203.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="日语听译学习 2022.2~2022.3",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "riyu-tingyi-xuexi-202202-202203=RiyuTingyiXuexi202202202203.__main__:main",
+            "RiyuTingyiXuexi202202202203=RiyuTingyiXuexi202202202203.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
