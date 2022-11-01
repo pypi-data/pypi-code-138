05db29b6a72dde1861e7ed0e6e3299613d08c059
Tom Forbes <tom@tomforb.es>
2022-11-01 13:22:28 +0000

    {"name":"riyu-201808-202008","version":"2022.11.1.1","file":"riyu_201808_202008-2022.11.1.1-py3-none-any.whl","path":"riyu-201808-202008/riyu_201808_202008-2022.11.1.1-py3-none-any.whl"}

---
+++ riyu-201808-202008/riyu_201808_202008-2022.11.1.1-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""riyu-201808-202008
+https://github.com/apachecn/riyu-201808-202008"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.1.1"
