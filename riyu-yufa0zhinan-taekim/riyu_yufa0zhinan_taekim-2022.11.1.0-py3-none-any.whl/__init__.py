4196a41a30198e904df5b67dc5b6b971f8d5b921
Tom Forbes <tom@tomforb.es>
2022-11-01 12:26:38 +0000

    {"name":"riyu-yufa0zhinan-taekim","version":"2022.11.1.0","file":"riyu_yufa0zhinan_taekim-2022.11.1.0-py3-none-any.whl","path":"riyu-yufa0zhinan-taekim/riyu_yufa0zhinan_taekim-2022.11.1.0-py3-none-any.whl"}

---
+++ riyu-yufa0zhinan-taekim/riyu_yufa0zhinan_taekim-2022.11.1.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""riyu-yufa0zhinan-taekim
+https://github.com/apachecn/riyu-yufa0zhinan-taekim"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.1.0"
