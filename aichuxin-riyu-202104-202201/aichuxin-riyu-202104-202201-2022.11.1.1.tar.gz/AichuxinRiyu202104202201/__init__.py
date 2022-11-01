2763187d5a8cccc396b53b729e4e3dbb4c425c00
Tom Forbes <tom@tomforb.es>
2022-11-01 12:38:58 +0000

    {"name":"aichuxin-riyu-202104-202201","version":"2022.11.1.1","file":"aichuxin-riyu-202104-202201-2022.11.1.1.tar.gz","path":"aichuxin-riyu-202104-202201/aichuxin-riyu-202104-202201-2022.11.1.1.tar.gz"}

---
+++ aichuxin-riyu-202104-202201/aichuxin-riyu-202104-202201-2022.11.1.1.tar.gz/AichuxinRiyu202104202201/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""aichuxin-riyu-202104-202201
+https://github.com/apachecn/aichuxin-riyu-202104-202201"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.1.1"
