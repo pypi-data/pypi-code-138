7e9701060578dc61c2837a5169d60e7bd34e6903
Tom Forbes <tom@tomforb.es>
2022-11-01 13:34:48 +0000

    {"name":"riyu-kaoshitong-202006-202107","version":"2022.11.1.1","file":"riyu-kaoshitong-202006-202107-2022.11.1.1.tar.gz","path":"riyu-kaoshitong-202006-202107/riyu-kaoshitong-202006-202107-2022.11.1.1.tar.gz"}

---
+++ riyu-kaoshitong-202006-202107/riyu-kaoshitong-202006-202107-2022.11.1.1.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import RiyuKaoshitong202006202107
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('RiyuKaoshitong202006202107'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="riyu-kaoshitong-202006-202107",
+    version=RiyuKaoshitong202006202107.__version__,
+    url="https://github.com/apachecn/riyu-kaoshitong-202006-202107",
+    author=RiyuKaoshitong202006202107.__author__,
+    author_email=RiyuKaoshitong202006202107.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="日语考试通 2020.6~2021.7",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "riyu-kaoshitong-202006-202107=RiyuKaoshitong202006202107.__main__:main",
+            "RiyuKaoshitong202006202107=RiyuKaoshitong202006202107.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
