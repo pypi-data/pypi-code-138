5bf4436b7d4d776dd12270cab223f58b237b7e7b
Tom Forbes <tom@tomforb.es>
2022-11-01 12:52:29 +0000

    {"name":"renmin-zhongguo-riwenban-201711-201805","version":"2022.11.1.1","file":"renmin_zhongguo_riwenban_201711_201805-2022.11.1.1-py3-none-any.whl","path":"renmin-zhongguo-riwenban-201711-201805/renmin_zhongguo_riwenban_201711_201805-2022.11.1.1-py3-none-any.whl"}

---
+++ renmin-zhongguo-riwenban-201711-201805/renmin_zhongguo_riwenban_201711_201805-2022.11.1.1-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""renmin-zhongguo-riwenban-201711-201805
+https://github.com/apachecn/renmin-zhongguo-riwenban-201711-201805"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.1.1"
