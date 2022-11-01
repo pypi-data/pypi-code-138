1cca53205f3daeb39ed0ae50b73915525607ab97
Tom Forbes <tom@tomforb.es>
2022-11-01 19:06:54 +0000

    {"name":"nhk-riyu-tingli-202109-202111","version":"2022.11.2.0","file":"nhk-riyu-tingli-202109-202111-2022.11.2.0.tar.gz","path":"nhk-riyu-tingli-202109-202111/nhk-riyu-tingli-202109-202111-2022.11.2.0.tar.gz"}

---
+++ nhk-riyu-tingli-202109-202111/nhk-riyu-tingli-202109-202111-2022.11.2.0.tar.gz/NhkRiyuTingli202109202111/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""nhk-riyu-tingli-202109-202111
+https://github.com/apachecn/nhk-riyu-tingli-202109-202111"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.2.0"
