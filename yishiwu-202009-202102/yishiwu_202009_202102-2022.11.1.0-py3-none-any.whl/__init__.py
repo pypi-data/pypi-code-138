0cf27c7cb47d7aa83797c4881c21c9801d64ba45
Tom Forbes <tom@tomforb.es>
2022-11-01 13:56:36 +0000

    {"name":"yishiwu-202009-202102","version":"2022.11.1.0","file":"yishiwu_202009_202102-2022.11.1.0-py3-none-any.whl","path":"yishiwu-202009-202102/yishiwu_202009_202102-2022.11.1.0-py3-none-any.whl"}

---
+++ yishiwu-202009-202102/yishiwu_202009_202102-2022.11.1.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yishiwu-202009-202102
+https://github.com/apachecn/yishiwu-202009-202102"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.1.0"
