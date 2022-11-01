111aa85525b0418beb77262d49335dad216e163d
Tom Forbes <tom@tomforb.es>
2022-11-01 18:42:39 +0000

    {"name":"chuxin-lianmeng-riyu-202009-202101","version":"2022.11.2.1","file":"chuxin-lianmeng-riyu-202009-202101-2022.11.2.1.tar.gz","path":"chuxin-lianmeng-riyu-202009-202101/chuxin-lianmeng-riyu-202009-202101-2022.11.2.1.tar.gz"}

---
+++ chuxin-lianmeng-riyu-202009-202101/chuxin-lianmeng-riyu-202009-202101-2022.11.2.1.tar.gz/ChuxinLianmengRiyu202009202101/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""chuxin-lianmeng-riyu-202009-202101
+https://github.com/apachecn/chuxin-lianmeng-riyu-202009-202101"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.2.1"
