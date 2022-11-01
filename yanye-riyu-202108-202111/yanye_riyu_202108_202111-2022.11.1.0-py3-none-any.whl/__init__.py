f2d57bd6fd2dfdb5795d278d09d625b2ff6f524b
Tom Forbes <tom@tomforb.es>
2022-11-01 13:53:29 +0000

    {"name":"yanye-riyu-202108-202111","version":"2022.11.1.0","file":"yanye_riyu_202108_202111-2022.11.1.0-py3-none-any.whl","path":"yanye-riyu-202108-202111/yanye_riyu_202108_202111-2022.11.1.0-py3-none-any.whl"}

---
+++ yanye-riyu-202108-202111/yanye_riyu_202108_202111-2022.11.1.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yanye-riyu-202108-202111
+https://github.com/apachecn/yanye-riyu-202108-202111"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.1.0"
