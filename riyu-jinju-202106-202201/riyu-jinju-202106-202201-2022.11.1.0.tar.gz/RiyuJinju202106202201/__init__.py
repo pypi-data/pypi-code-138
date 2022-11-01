97316a5ed3b434e2c30a47cc92684948feb894d2
Tom Forbes <tom@tomforb.es>
2022-11-01 12:18:27 +0000

    {"name":"riyu-jinju-202106-202201","version":"2022.11.1.0","file":"riyu-jinju-202106-202201-2022.11.1.0.tar.gz","path":"riyu-jinju-202106-202201/riyu-jinju-202106-202201-2022.11.1.0.tar.gz"}

---
+++ riyu-jinju-202106-202201/riyu-jinju-202106-202201-2022.11.1.0.tar.gz/RiyuJinju202106202201/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""riyu-jinju-202106-202201
+https://github.com/apachecn/riyu-jinju-202106-202201"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.1.0"
