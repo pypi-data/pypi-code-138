be9b15759fd71dc3b80ddad5c902b0f958e31fa7
Tom Forbes <tom@tomforb.es>
2022-11-01 18:50:48 +0000

    {"name":"chuxin-riyu-lianmeng-202204-202206","version":"2022.11.2.0","file":"chuxin-riyu-lianmeng-202204-202206-2022.11.2.0.tar.gz","path":"chuxin-riyu-lianmeng-202204-202206/chuxin-riyu-lianmeng-202204-202206-2022.11.2.0.tar.gz"}

---
+++ chuxin-riyu-lianmeng-202204-202206/chuxin-riyu-lianmeng-202204-202206-2022.11.2.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import ChuxinRiyuLianmeng202204202206
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('ChuxinRiyuLianmeng202204202206'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="chuxin-riyu-lianmeng-202204-202206",
+    version=ChuxinRiyuLianmeng202204202206.__version__,
+    url="https://github.com/apachecn/chuxin-riyu-lianmeng-202204-202206",
+    author=ChuxinRiyuLianmeng202204202206.__author__,
+    author_email=ChuxinRiyuLianmeng202204202206.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="初心日语联盟 2022.4~2022.6",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "chuxin-riyu-lianmeng-202204-202206=ChuxinRiyuLianmeng202204202206.__main__:main",
+            "ChuxinRiyuLianmeng202204202206=ChuxinRiyuLianmeng202204202206.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
