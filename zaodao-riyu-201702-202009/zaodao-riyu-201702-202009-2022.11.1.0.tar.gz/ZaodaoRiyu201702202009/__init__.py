94a3a4517c1c449663e13805041e4b8fd5bfbbd6
Tom Forbes <tom@tomforb.es>
2022-11-01 14:02:59 +0000

    {"name":"zaodao-riyu-201702-202009","version":"2022.11.1.0","file":"zaodao-riyu-201702-202009-2022.11.1.0.tar.gz","path":"zaodao-riyu-201702-202009/zaodao-riyu-201702-202009-2022.11.1.0.tar.gz"}

---
+++ zaodao-riyu-201702-202009/zaodao-riyu-201702-202009-2022.11.1.0.tar.gz/ZaodaoRiyu201702202009/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""zaodao-riyu-201702-202009
+https://github.com/apachecn/zaodao-riyu-201702-202009"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.1.0"
