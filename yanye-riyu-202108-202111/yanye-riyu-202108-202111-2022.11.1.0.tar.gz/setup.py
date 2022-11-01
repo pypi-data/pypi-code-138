db16058597db8279f937cb9f40d9d297c552cd71
Tom Forbes <tom@tomforb.es>
2022-11-01 13:53:31 +0000

    {"name":"yanye-riyu-202108-202111","version":"2022.11.1.0","file":"yanye-riyu-202108-202111-2022.11.1.0.tar.gz","path":"yanye-riyu-202108-202111/yanye-riyu-202108-202111-2022.11.1.0.tar.gz"}

---
+++ yanye-riyu-202108-202111/yanye-riyu-202108-202111-2022.11.1.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import YanyeRiyu202108202111
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('YanyeRiyu202108202111'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="yanye-riyu-202108-202111",
+    version=YanyeRiyu202108202111.__version__,
+    url="https://github.com/apachecn/yanye-riyu-202108-202111",
+    author=YanyeRiyu202108202111.__author__,
+    author_email=YanyeRiyu202108202111.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="言叶日语 2021.8~2021.11",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "yanye-riyu-202108-202111=YanyeRiyu202108202111.__main__:main",
+            "YanyeRiyu202108202111=YanyeRiyu202108202111.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
