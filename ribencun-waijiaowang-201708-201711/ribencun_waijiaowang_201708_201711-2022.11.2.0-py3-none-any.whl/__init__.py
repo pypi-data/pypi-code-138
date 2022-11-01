00033766fb6b1f854b302ce9ef60e3018bfb6a6b
Tom Forbes <tom@tomforb.es>
2022-11-01 19:37:55 +0000

    {"name":"ribencun-waijiaowang-201708-201711","version":"2022.11.2.0","file":"ribencun_waijiaowang_201708_201711-2022.11.2.0-py3-none-any.whl","path":"ribencun-waijiaowang-201708-201711/ribencun_waijiaowang_201708_201711-2022.11.2.0-py3-none-any.whl"}

---
+++ ribencun-waijiaowang-201708-201711/ribencun_waijiaowang_201708_201711-2022.11.2.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ribencun-waijiaowang-201708-201711
+https://github.com/apachecn/ribencun-waijiaowang-201708-201711"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.2.0"
