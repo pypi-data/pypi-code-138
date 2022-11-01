65ea405838172b25f6145492f6451665bf7875a9
Tom Forbes <tom@tomforb.es>
2022-11-01 12:50:28 +0000

    {"name":"furi-liuxue-zhinan-201801-201905","version":"2022.11.1.1","file":"furi_liuxue_zhinan_201801_201905-2022.11.1.1-py3-none-any.whl","path":"furi-liuxue-zhinan-201801-201905/furi_liuxue_zhinan_201801_201905-2022.11.1.1-py3-none-any.whl"}

---
+++ furi-liuxue-zhinan-201801-201905/furi_liuxue_zhinan_201801_201905-2022.11.1.1-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""furi-liuxue-zhinan-201801-201905
+https://github.com/apachecn/furi-liuxue-zhinan-201801-201905"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.1.1"
