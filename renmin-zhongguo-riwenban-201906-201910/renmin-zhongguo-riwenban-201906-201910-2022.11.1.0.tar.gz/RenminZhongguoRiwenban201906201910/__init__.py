0971c9bff413b055c1a63a409f0d80d2cc459269
Tom Forbes <tom@tomforb.es>
2022-11-01 11:54:02 +0000

    {"name":"renmin-zhongguo-riwenban-201906-201910","version":"2022.11.1.0","file":"renmin-zhongguo-riwenban-201906-201910-2022.11.1.0.tar.gz","path":"renmin-zhongguo-riwenban-201906-201910/renmin-zhongguo-riwenban-201906-201910-2022.11.1.0.tar.gz"}

---
+++ renmin-zhongguo-riwenban-201906-201910/renmin-zhongguo-riwenban-201906-201910-2022.11.1.0.tar.gz/RenminZhongguoRiwenban201906201910/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""renmin-zhongguo-riwenban-201906-201910
+https://github.com/apachecn/renmin-zhongguo-riwenban-201906-201910"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.1.0"
