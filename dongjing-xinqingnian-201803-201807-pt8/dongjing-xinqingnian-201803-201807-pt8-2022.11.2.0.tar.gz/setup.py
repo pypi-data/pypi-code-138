d33c8e6cfbff497a48760c814d6b5c88099a1896
Tom Forbes <tom@tomforb.es>
2022-11-02 09:25:48 +0000

    {"name":"dongjing-xinqingnian-201803-201807-pt8","version":"2022.11.2.0","file":"dongjing-xinqingnian-201803-201807-pt8-2022.11.2.0.tar.gz","path":"dongjing-xinqingnian-201803-201807-pt8/dongjing-xinqingnian-201803-201807-pt8-2022.11.2.0.tar.gz"}

---
+++ dongjing-xinqingnian-201803-201807-pt8/dongjing-xinqingnian-201803-201807-pt8-2022.11.2.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import DongjingXinqingnian201803201807Pt8
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('DongjingXinqingnian201803201807Pt8'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="dongjing-xinqingnian-201803-201807-pt8",
+    version=DongjingXinqingnian201803201807Pt8.__version__,
+    url="https://github.com/apachecn/dongjing-xinqingnian-201803-201807-pt8",
+    author=DongjingXinqingnian201803201807Pt8.__author__,
+    author_email=DongjingXinqingnian201803201807Pt8.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="东京新青年 2018.3~2018.7 PT8",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "dongjing-xinqingnian-201803-201807-pt8=DongjingXinqingnian201803201807Pt8.__main__:main",
+            "DongjingXinqingnian201803201807Pt8=DongjingXinqingnian201803201807Pt8.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
