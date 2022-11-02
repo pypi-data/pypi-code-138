3e75dc6a7fb73496fd214f16e721585cb5c4924a
Tom Forbes <tom@tomforb.es>
2022-11-02 12:03:21 +0000

    {"name":"riyu-xuexi-jiaming-biaozhu-202112-202203","version":"2022.11.2.0","file":"riyu-xuexi-jiaming-biaozhu-202112-202203-2022.11.2.0.tar.gz","path":"riyu-xuexi-jiaming-biaozhu-202112-202203/riyu-xuexi-jiaming-biaozhu-202112-202203-2022.11.2.0.tar.gz"}

---
+++ riyu-xuexi-jiaming-biaozhu-202112-202203/riyu-xuexi-jiaming-biaozhu-202112-202203-2022.11.2.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import RiyuXuexiJiamingBiaozhu202112202203
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('RiyuXuexiJiamingBiaozhu202112202203'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="riyu-xuexi-jiaming-biaozhu-202112-202203",
+    version=RiyuXuexiJiamingBiaozhu202112202203.__version__,
+    url="https://github.com/apachecn/riyu-xuexi-jiaming-biaozhu-202112-202203",
+    author=RiyuXuexiJiamingBiaozhu202112202203.__author__,
+    author_email=RiyuXuexiJiamingBiaozhu202112202203.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="日语学习假名标注 2021.12~2022.3",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "riyu-xuexi-jiaming-biaozhu-202112-202203=RiyuXuexiJiamingBiaozhu202112202203.__main__:main",
+            "RiyuXuexiJiamingBiaozhu202112202203=RiyuXuexiJiamingBiaozhu202112202203.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
