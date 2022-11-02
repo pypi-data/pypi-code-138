86e264516cdf7149b166bc0524941dfd3a36b1a5
Tom Forbes <tom@tomforb.es>
2022-11-02 11:26:38 +0000

    {"name":"hujiang-riyu-201912-202007","version":"2022.11.2.0","file":"hujiang_riyu_201912_202007-2022.11.2.0-py3-none-any.whl","path":"hujiang-riyu-201912-202007/hujiang_riyu_201912_202007-2022.11.2.0-py3-none-any.whl"}

---
+++ hujiang-riyu-201912-202007/hujiang_riyu_201912_202007-2022.11.2.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""hujiang-riyu-201912-202007
+https://github.com/apachecn/hujiang-riyu-201912-202007"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.2.0"
