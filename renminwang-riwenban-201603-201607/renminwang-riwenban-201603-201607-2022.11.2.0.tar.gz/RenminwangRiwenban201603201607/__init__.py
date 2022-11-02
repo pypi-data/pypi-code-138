23ceb4a88a06ac92570473031c7fa2685ebc1d5e
Tom Forbes <tom@tomforb.es>
2022-11-02 11:44:03 +0000

    {"name":"renminwang-riwenban-201603-201607","version":"2022.11.2.0","file":"renminwang-riwenban-201603-201607-2022.11.2.0.tar.gz","path":"renminwang-riwenban-201603-201607/renminwang-riwenban-201603-201607-2022.11.2.0.tar.gz"}

---
+++ renminwang-riwenban-201603-201607/renminwang-riwenban-201603-201607-2022.11.2.0.tar.gz/RenminwangRiwenban201603201607/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""renminwang-riwenban-201603-201607
+https://github.com/apachecn/renminwang-riwenban-201603-201607"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.2.0"
