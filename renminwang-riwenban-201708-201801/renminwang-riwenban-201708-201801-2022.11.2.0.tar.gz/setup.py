37b57c8a4178888334c0cb9ac848c448b2e2c3f9
Tom Forbes <tom@tomforb.es>
2022-11-02 11:51:06 +0000

    {"name":"renminwang-riwenban-201708-201801","version":"2022.11.2.0","file":"renminwang-riwenban-201708-201801-2022.11.2.0.tar.gz","path":"renminwang-riwenban-201708-201801/renminwang-riwenban-201708-201801-2022.11.2.0.tar.gz"}

---
+++ renminwang-riwenban-201708-201801/renminwang-riwenban-201708-201801-2022.11.2.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import RenminwangRiwenban201708201801
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('RenminwangRiwenban201708201801'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="renminwang-riwenban-201708-201801",
+    version=RenminwangRiwenban201708201801.__version__,
+    url="https://github.com/apachecn/renminwang-riwenban-201708-201801",
+    author=RenminwangRiwenban201708201801.__author__,
+    author_email=RenminwangRiwenban201708201801.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="人民网日文版 2017.8~2018.1",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "renminwang-riwenban-201708-201801=RenminwangRiwenban201708201801.__main__:main",
+            "RenminwangRiwenban201708201801=RenminwangRiwenban201708201801.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
