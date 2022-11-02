3e5c48bbc9a83bb1341f95fd08258c782732e424
Tom Forbes <tom@tomforb.es>
2022-11-02 11:58:50 +0000

    {"name":"riyu-xuexi-jiaming-biaozhu-201707-201712","version":"2022.11.2.1","file":"riyu-xuexi-jiaming-biaozhu-201707-201712-2022.11.2.1.tar.gz","path":"riyu-xuexi-jiaming-biaozhu-201707-201712/riyu-xuexi-jiaming-biaozhu-201707-201712-2022.11.2.1.tar.gz"}

---
+++ riyu-xuexi-jiaming-biaozhu-201707-201712/riyu-xuexi-jiaming-biaozhu-201707-201712-2022.11.2.1.tar.gz/RiyuXuexiJiamingBiaozhu201707201712/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""riyu-xuexi-jiaming-biaozhu-201707-201712
+https://github.com/apachecn/riyu-xuexi-jiaming-biaozhu-201707-201712"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.2.1"
