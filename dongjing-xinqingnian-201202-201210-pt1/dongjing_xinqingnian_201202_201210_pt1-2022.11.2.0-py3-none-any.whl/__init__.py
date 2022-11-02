ba712b99a0a90766aad2195ac67ebbd65044131a
Tom Forbes <tom@tomforb.es>
2022-11-02 10:32:52 +0000

    {"name":"dongjing-xinqingnian-201202-201210-pt1","version":"2022.11.2.0","file":"dongjing_xinqingnian_201202_201210_pt1-2022.11.2.0-py3-none-any.whl","path":"dongjing-xinqingnian-201202-201210-pt1/dongjing_xinqingnian_201202_201210_pt1-2022.11.2.0-py3-none-any.whl"}

---
+++ dongjing-xinqingnian-201202-201210-pt1/dongjing_xinqingnian_201202_201210_pt1-2022.11.2.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""dongjing-xinqingnian-201202-201210-pt1
+https://github.com/apachecn/dongjing-xinqingnian-201202-201210-pt1"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.2.0"
