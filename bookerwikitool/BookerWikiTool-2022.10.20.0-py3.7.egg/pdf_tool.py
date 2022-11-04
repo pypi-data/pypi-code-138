b5290066d71b4830dc28ada37f2327f369f420ea
Tom Forbes <tom@tomforb.es>
2022-11-04 08:33:39 +0000

    {"name":"bookerwikitool","version":"2022.10.20.0","file":"BookerWikiTool-2022.10.20.0-py3.7.egg","path":"bookerwikitool/BookerWikiTool-2022.10.20.0-py3.7.egg"}

---
+++ bookerwikitool/BookerWikiTool-2022.10.20.0-py3.7.egg/pdf_tool.py
@@ -0,0 +1,55 @@
+import fitz
+from os import path
+import re
+import os
+import traceback
+
+def ext_pdf(args):
+    if path.isdir(args.fname):
+        ext_pdf_dir(args)
+    else:
+        ext_pdf_file(args)
+
+def ext_pdf_dir(args):
+    dir = args.fname
+    for fname in os.listdir(dir):
+        try:
+            ffname = path.join(dir, fname)
+            args.fname = ffname
+            ext_pdf_file(args)
+        except: traceback.print_exc()
+
+def ext_pdf_file(args):
+    fname, dir = args.fname, args.dir
+    if not fname.endswith('.pdf'):
+        print('请提供 PDF 文件')
+        return
+    print(f'file: {fname}')
+    title = path.basename(fname)[:-4]
+
+    doc = fitz.open(fname)
+    lp = len(str(len(doc)))
+    for ip, p in enumerate(doc):
+        print(f'page: {ip + 1}')
+        
+        # 判断是否整页截图
+        if args.whole:
+            img = p.get_pixmap()
+            imgname = path.join(dir, f'{title}_{ip+1:0{lp}d}.png')
+            print(f'save: {imgname}')
+            img.writePNG(imgname)
+            continue
+        
+        imgs = p.get_images()
+        limg = len(str(len(imgs)))
+        for ii, info in enumerate(imgs):
+            xref = info[0]
+            print(f'img: {ii + 1}, xref: {xref}')
+            img = fitz.Pixmap(doc, xref)
+            if img.n >= 5:
+                pix = fitz.Pixmap(fitz.csRGB, pix)
+            imgname = path.join(dir, f'{title}_{ip+1:0{lp}d}_{ii+1:0{limg}d}.png')
+            print(f'save: {imgname}')
+            img.writePNG(imgname)
+    
+    doc.close()
