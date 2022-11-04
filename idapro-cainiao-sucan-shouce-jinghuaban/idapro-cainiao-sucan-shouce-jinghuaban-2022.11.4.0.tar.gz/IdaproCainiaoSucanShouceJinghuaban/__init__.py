257335130e71b23948e942fbbeffd3357aa6be78
Tom Forbes <tom@tomforb.es>
2022-11-04 06:40:49 +0000

    {"name":"idapro-cainiao-sucan-shouce-jinghuaban","version":"2022.11.4.0","file":"idapro-cainiao-sucan-shouce-jinghuaban-2022.11.4.0.tar.gz","path":"idapro-cainiao-sucan-shouce-jinghuaban/idapro-cainiao-sucan-shouce-jinghuaban-2022.11.4.0.tar.gz"}

---
+++ idapro-cainiao-sucan-shouce-jinghuaban/idapro-cainiao-sucan-shouce-jinghuaban-2022.11.4.0.tar.gz/IdaproCainiaoSucanShouceJinghuaban/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""idapro-cainiao-sucan-shouce-jinghuaban
+https://github.com/apachecn/idapro-cainiao-sucan-shouce-jinghuaban"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.4.0"
