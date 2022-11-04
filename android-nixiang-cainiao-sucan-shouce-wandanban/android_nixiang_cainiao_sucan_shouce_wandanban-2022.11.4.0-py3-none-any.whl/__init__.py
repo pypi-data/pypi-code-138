588af237a7d646397e0b8ba174c1d2427ba61d29
Tom Forbes <tom@tomforb.es>
2022-11-04 06:14:40 +0000

    {"name":"android-nixiang-cainiao-sucan-shouce-wandanban","version":"2022.11.4.0","file":"android_nixiang_cainiao_sucan_shouce_wandanban-2022.11.4.0-py3-none-any.whl","path":"android-nixiang-cainiao-sucan-shouce-wandanban/android_nixiang_cainiao_sucan_shouce_wandanban-2022.11.4.0-py3-none-any.whl"}

---
+++ android-nixiang-cainiao-sucan-shouce-wandanban/android_nixiang_cainiao_sucan_shouce_wandanban-2022.11.4.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""android-nixiang-cainiao-sucan-shouce-wandanban
+https://github.com/apachecn/android-nixiang-cainiao-sucan-shouce-wandanban"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.4.0"
