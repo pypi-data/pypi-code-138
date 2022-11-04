4d9dc45da3fc2c6091befea5692778ecf263585f
Tom Forbes <tom@tomforb.es>
2022-11-04 08:35:12 +0000

    {"name":"bookerdownloadtool","version":"2022.8.19.0","file":"BookerDownloadTool-2022.8.19.0-py3.7.egg","path":"bookerdownloadtool/BookerDownloadTool-2022.8.19.0-py3.7.egg"}

---
+++ bookerdownloadtool/BookerDownloadTool-2022.8.19.0-py3.7.egg/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from .zhihu_ques_sele import *
+from .lightnovel import *
+from .dl_gh_book import *
+from .bili import *
+
+"""BookerDownloadTool
+https://github.com/apachecnBookerDownloadTool"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "SATA"
+__version__ = "2022.8.19.0"
