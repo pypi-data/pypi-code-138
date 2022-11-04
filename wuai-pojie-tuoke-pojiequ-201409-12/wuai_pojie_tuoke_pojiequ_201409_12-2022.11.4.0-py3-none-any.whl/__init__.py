cc32b3b68d99944a2ead8b3a159684322a43d6b5
Tom Forbes <tom@tomforb.es>
2022-11-04 06:48:35 +0000

    {"name":"wuai-pojie-tuoke-pojiequ-201409-12","version":"2022.11.4.0","file":"wuai_pojie_tuoke_pojiequ_201409_12-2022.11.4.0-py3-none-any.whl","path":"wuai-pojie-tuoke-pojiequ-201409-12/wuai_pojie_tuoke_pojiequ_201409_12-2022.11.4.0-py3-none-any.whl"}

---
+++ wuai-pojie-tuoke-pojiequ-201409-12/wuai_pojie_tuoke_pojiequ_201409_12-2022.11.4.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""wuai-pojie-tuoke-pojiequ-201409-12
+https://github.com/apachecn/wuai-pojie-tuoke-pojiequ-201409-12"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.4.0"
