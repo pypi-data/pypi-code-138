6b5254a0a5447405cbd9672ea54090d5c4990ec2
Tom Forbes <tom@tomforb.es>
2022-11-04 06:48:01 +0000

    {"name":"wuai-pojie-tuoke-pojiequ-201401-04","version":"2022.11.4.0","file":"wuai-pojie-tuoke-pojiequ-201401-04-2022.11.4.0.tar.gz","path":"wuai-pojie-tuoke-pojiequ-201401-04/wuai-pojie-tuoke-pojiequ-201401-04-2022.11.4.0.tar.gz"}

---
+++ wuai-pojie-tuoke-pojiequ-201401-04/wuai-pojie-tuoke-pojiequ-201401-04-2022.11.4.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import WuaiPojieTuokePojiequ20140104
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('WuaiPojieTuokePojiequ20140104'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="wuai-pojie-tuoke-pojiequ-201401-04",
+    version=WuaiPojieTuokePojiequ20140104.__version__,
+    url="https://github.com/apachecn/wuai-pojie-tuoke-pojiequ-201401-04",
+    author=WuaiPojieTuokePojiequ20140104.__author__,
+    author_email=WuaiPojieTuokePojiequ20140104.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="吾爱破解脱壳破解区 2014.1~4",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "wuai-pojie-tuoke-pojiequ-201401-04=WuaiPojieTuokePojiequ20140104.__main__:main",
+            "WuaiPojieTuokePojiequ20140104=WuaiPojieTuokePojiequ20140104.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
